3978a3434a17e898afa847e04ca5deebc3d2f98f
C3NZ <cenz@cenz.io>
2020-04-01 12:59:48 -0700

    [fix] the pre commit to just not check headers of our files since I don't want to include files globally.

---
+++ engine/src/core/Log.cpp
@@ -0,0 +1,21 @@
+#include "Log.h"
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace engine {
+
+std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+void Log::Init() {
+  spdlog::set_pattern("%^[%T] %n: %v%$");
+  s_CoreLogger = spdlog::stdout_color_mt("Engine");
+  s_CoreLogger->set_level(spdlog::level::trace);
+
+  s_ClientLogger = spdlog::stdout_color_mt("App");
+
+  s_CoreLogger->set_level(spdlog::level::trace);
+}
+
+}  // namespace engine
--- engine/src/core/Log.h
+++ engine/src/core/Log.h
@@ -1,4 +1,56 @@
 #ifndef ENGINE_SRC_CORE_LOG_H_
 #define ENGINE_SRC_CORE_LOG_H_
 
+#include <memory>
+
+#include "Core.h"
+
+#include "spdlog/spdlog.h"
+
+namespace engine {
+
+class ENGINE_API Log {
+ public:
+  static void Init();
+
+  inline static std::shared_ptr<spdlog::logger>& GetCoreLogger()
+      { return s_CoreLogger; }
+  inline static std::shared_ptr<spdlog::logger>& GetClientLogger()
+      { return s_ClientLogger; }
+
+ private:
+  static std::shared_ptr<spdlog::logger> s_CoreLogger;
+  static std::shared_ptr<spdlog::logger> s_ClientLogger;
+};
+
+}  // namespace engine
+
+// TODO(C3NZ): Evaluate if there's a better way to implement our logger.
+// Ideally, I would like to attach these macros as functions of the Log class,
+// but am not sure how to handle the variadic parameters as of now.
+
+// Engine log macros
+#define ENGINE_CORE_TRACE(...) \
+    ::engine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define ENGINE_CORE_INFO(...)  \
+    ::engine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define ENGINE_CORE_WARN(...)  \
+    ::engine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define ENGINE_CORE_ERROR(...) \
+    ::engine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define ENGINE_CORE_FATAL(...) \
+    ::engine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define ENGINE_CLIENT_TRACE(...) \
+    ::engine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define ENGINE_CLIENT_INFO(...)  \
+    ::engine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define ENGINE_CLIENT_WARN(...)  \
+    ::engine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define ENGINE_CLIENT_ERROR(...) \
+    ::engine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define ENGINE_CLIENT_FATAL(...) \
+    ::engine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
 #endif  // ENGINE_SRC_CORE_LOG_H_
