dd2aded4b6633592e4ff3ceffa49620cbfdab0b4
C3NZ <cenz@cenz.io>
2020-04-01 13:03:12 -0700

    [tidy] up our application file to adhere to google styling.

--- engine/src/core/Application.cpp
+++ engine/src/core/Application.cpp
@@ -1,19 +1,11 @@
 #include "Application.h"
 
-#include <iostream>
-
 namespace engine {
-  Application::Application() {
+  Application::Application() {}
 
-  }
-
-  Application::~Application() {
-
-  }
+  Application::~Application() {}
 
   void Application::Run() {
-    while (true) {
-      std::cout << "Hello";
-    }
+    while (true) {}
   }
 }
