d0fbed5a55737f0c623178b712092d9011251f25
C3NZ <cenz@cenz.io>
2020-04-01 17:34:11 -0700

    [add] the ability for devs to pass in flags that allow the script to take in a flag that allows users to differentiate between different builds.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,3 +1,13 @@
-cmake .
+if [ "$1" = "Release" ] || [ "$1" = "Debug" ]; then
+    echo "Compiling a $1 build."
+    cmake -DCMAKE_BUILD_TYPE="$1" -DDISTRIBUTION_BUILD=False .
+elif [ "$1" = "Dist" ]; then
+    echo "Compiling a Distribution build."
+    cmake -DCMAKE_BUILD_TYPE="Release" -DDISTRIBUTION_BUILD=True .
+else
+    echo "Incorrect build type passed."
+    exit
+fi
+
 make
-./bin/app
+./"$1"/bin/app
