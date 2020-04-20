2d47e798e81a5545da4fdfc69ec2ad3050d550ee
C3NZ <cenz@cenz.io>
2020-04-19 22:35:33 -0700

    [fix] compile and run so that each release that isn't a distribution build will allow us to compile all of the platform code.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -2,7 +2,10 @@ export CC=gcc CXX=g++
 
 if [ "$1" = "Release" ] || [ "$1" = "Debug" ]; then
     echo "Compiling a $1 build."
-    cmake -DCMAKE_BUILD_TYPE="$1" -DDISTRIBUTION_BUILD=False .
+    cmake -DCMAKE_BUILD_TYPE="$1" \
+        -DDISTRIBUTION_BUILD=False \
+        -DENGINE_DEVELOPMENT_MODE=True \
+        .
 elif [ "$1" = "Dist" ]; then
     echo "Compiling a Distribution build."
     cmake -DCMAKE_BUILD_TYPE="Release" -DDISTRIBUTION_BUILD=True .
