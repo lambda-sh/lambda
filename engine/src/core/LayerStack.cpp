aaefcc0add0673de00672593073767a5d0476adf
C3NZ <cenz@cenz.io>
2020-04-21 13:11:14 -0700

    [add] the LayerStack which manages how layers are processed and the precedence in which they're stored for processing.

---
+++ engine/src/core/LayerStack.cpp
@@ -0,0 +1,44 @@
+#include "core/LayerStack.h"
+
+#include <vector>
+
+namespace engine {
+
+LayerStack::LayerStack() {
+  layer_insert_location_ = layers_.begin();
+}
+
+// Layers are destroyed as soon as the layer stack also is.
+LayerStack::~LayerStack() {
+  for (Layer* layer : layers_) {
+    delete layer;
+  }
+}
+
+void LayerStack::PushLayer(Layer* layer) {
+  layer_insert_location_ = layers_.emplace(layer_insert_location_, layer);
+}
+
+// Layers will always be pushed into the back of the list as the last thing to
+// be rendered/handled. This ensures that overlays are always on top of layers
+//
+void LayerStack::PushOverlay(Layer* overlay) {
+  layers_.emplace_back(overlay);
+}
+
+void LayerStack::PopLayer(Layer* layer) {
+  auto it = std::find(layers_.begin(), layers_.end(), layer);
+  if (it != layers_.end()) {
+    layers_.erase(it);
+    layer_insert_location_--;
+  }
+}
+
+void LayerStack::PopOverlay(Layer* overlay) {
+  auto it = std::find(layers_.begin(), layers_.end(), overlay);
+  if (it != layers_.end()) {
+    layers_.erase(it);
+  }
+}
+
+}  // namespace engine
---
+++ engine/src/core/LayerStack.h
@@ -0,0 +1,36 @@
+#ifndef ENGINE_SRC_CORE_LAYERSTACK_H_
+#define ENGINE_SRC_CORE_LAYERSTACK_H_
+
+#include <vector>
+
+#include "core/Core.h"
+#include "core/Layer.h"
+
+namespace engine {
+
+class ENGINE_API LayerStack {
+ public:
+  LayerStack();
+  ~LayerStack();
+
+  // Push a layer on to the Layer stack.
+  void PushLayer(Layer* layer);
+  // Push an overlay on the layer stack.
+  void PushOverlay(Layer* overlay);
+  // Pop a layer off of the Layer stack.
+  void PopLayer(Layer* layer);
+  // Pop an overlay off of to the Layer stack.
+  void PopOverlay(Layer* layer);
+
+  // Get the beginning of the layer stack.
+  std::vector<Layer*>::iterator begin() { return layers_.begin(); }
+  // Get the end of the layer stack.
+  std::vector<Layer*>::iterator end() { return layers_.end(); }
+ private:
+  std::vector<Layer*> layers_;
+  std::vector<Layer*>::iterator layer_insert_location_;
+};
+
+}  // namespace engine
+
+#endif  // ENGINE_SRC_CORE_LAYERSTACK_H_
