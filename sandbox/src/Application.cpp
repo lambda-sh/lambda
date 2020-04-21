a8b14f90423786e972088874dec2c72b9c139db4
C3NZ <cenz@cenz.io>
2020-04-21 13:16:04 -0700

    [add] a layer to our application and then push it on to the layer stack.

--- sandbox/src/Application.cpp
+++ sandbox/src/Application.cpp
@@ -1,8 +1,23 @@
 #include "Engine.h"
 
+class ExampleLayer : public engine::Layer {
+ public:
+  ExampleLayer() : Layer("Example") {}
+
+  void OnUpdate() override {
+    ENGINE_CLIENT_INFO("ExampleLayer::Update");
+  }
+
+  void OnEvent(engine::events::Event* event) override {
+    ENGINE_CLIENT_TRACE("{0}", *event);
+  }
+};
+
 class Sandbox : public engine::Application {
  public:
-  Sandbox() {}
+  Sandbox() {
+    PushLayer(new ExampleLayer());
+  }
 
   ~Sandbox() {}
 };
