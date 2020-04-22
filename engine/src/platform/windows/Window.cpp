6da2e94303e686e32249584f66ff493333891428
C3NZ <cenz@cenz.io>
2020-04-21 17:36:49 -0700

    [tidy] up the window implementation.

--- engine/src/platform/windows/Window.cpp
+++ engine/src/platform/windows/Window.cpp
@@ -69,19 +69,19 @@ void WindowImplementation::Init(const engine::WindowProperties& properties) {
   // Initialize GLFW
   glfwMakeContextCurrent(window_);
   glfwSetWindowUserPointer(window_, &properties_);
-  SetVSync(true);
+  SetVerticalSync(true);
 
   glfwSetWindowSizeCallback(
       window_,
       [](GLFWwindow* window, int width, int height) {
-          Properties& properties =
-              *static_cast<Properties*>(glfwGetWindowUserPointer(window));
+          Properties* properties =
+              static_cast<Properties*>(glfwGetWindowUserPointer(window));
 
           events::WindowResizeEvent event(width, height);
-          properties.Width = width;
-          properties.Height = height;
+          properties->Width = width;
+          properties->Height = height;
 
-          properties.EventCallback(&event);
+          properties->EventCallback(&event);
       });
 
   glfwSetWindowCloseCallback(
@@ -97,26 +97,26 @@ void WindowImplementation::Init(const engine::WindowProperties& properties) {
   glfwSetKeyCallback(
       window_,
       [](GLFWwindow* window, int key, int scancode, int action, int mods) {
-        Properties& properties =
-            *static_cast<Properties*>(glfwGetWindowUserPointer(window));
+        Properties* properties =
+            static_cast<Properties*>(glfwGetWindowUserPointer(window));
 
         switch (action) {
           case GLFW_PRESS:
           {
             events::KeyPressedEvent event(key, 0);
-            properties.EventCallback(&event);
+            properties->EventCallback(&event);
             break;
           }
           case GLFW_RELEASE:
           {
             events::KeyReleasedEvent event(key);
-            properties.EventCallback(&event);
+            properties->EventCallback(&event);
             break;
           }
           case GLFW_REPEAT:
           {
             events::KeyPressedEvent event(key, 1);
-            properties.EventCallback(&event);
+            properties->EventCallback(&event);
             break;
           }
         }
@@ -125,20 +125,20 @@ void WindowImplementation::Init(const engine::WindowProperties& properties) {
   glfwSetMouseButtonCallback(
       window_,
       [](GLFWwindow* window, int button, int action, int mods) {
-        Properties& properties =
-            *static_cast<Properties*>(glfwGetWindowUserPointer(window));
+        Properties* properties =
+            static_cast<Properties*>(glfwGetWindowUserPointer(window));
 
         switch (action) {
           case GLFW_PRESS:
           {
             events::MouseButtonPressedEvent event(button);
-            properties.EventCallback(&event);
+            properties->EventCallback(&event);
             break;
           }
           case GLFW_RELEASE:
           {
             events::MouseButtonReleasedEvent event(button);
-            properties.EventCallback(&event);
+            properties->EventCallback(&event);
             break;
           }
         }
@@ -152,7 +152,7 @@ void WindowImplementation::Init(const engine::WindowProperties& properties) {
 
         events::MouseScrolledEvent event(
             static_cast<float>(xOffset), static_cast<float>(yOffset));
-        properties.EventCallback(&event);
+        properties->EventCallback(&event);
       });
 
   glfwSetCursorPosCallback(
@@ -163,7 +163,7 @@ void WindowImplementation::Init(const engine::WindowProperties& properties) {
 
         events::MouseMovedEvent event(
             static_cast<float>(xPosition), static_cast<float>(yPosition));
-        properties.EventCallback(&event);
+        properties->EventCallback(&event);
       });
 }
 
