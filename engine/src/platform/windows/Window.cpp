9e33dd2efe11bcc3cc767644a89978c308a55548
C3NZ <cenz@cenz.io>
2020-04-22 20:20:08 -0700

    [add] char callback to handle keys typed and incorporate glad now that glfw doesn't provide opengl headers.

--- engine/src/platform/windows/Window.cpp
+++ engine/src/platform/windows/Window.cpp
@@ -2,6 +2,7 @@
 
 #include "platform/windows/Window.h"
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include "core/Core.h"
@@ -122,6 +123,16 @@ void WindowImplementation::Init(const engine::WindowProperties& properties) {
         }
       });
 
+  glfwSetCharCallback(
+      window_,
+      [](GLFWwindow* window, unsigned int character) {
+        Properties* properties =
+            static_cast<Properties*>(glfwGetWindowUserPointer(window));
+
+        events::KeyTypedEvent event(character);
+        properties->EventCallback(&event);
+      });
+
   glfwSetMouseButtonCallback(
       window_,
       [](GLFWwindow* window, int button, int action, int mods) {
@@ -176,6 +187,8 @@ void WindowImplementation::Shutdown() {
 void WindowImplementation::OnUpdate() {
   glfwPollEvents();
   glfwSwapBuffers(window_);
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
 }
 
 // Setup the current window to use or not use Vsync.
