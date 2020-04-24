fba29ca14a69128575e0fff34f76a6db57e650b1
C3NZ <cenz@cenz.io>
2020-04-23 20:35:42 -0700

    [add] a generic Input implementation that enables us to specify platform specific implementations for input.

---
+++ engine/src/core/Input.h
@@ -0,0 +1,40 @@
+#ifndef ENGINE_SRC_CORE_INPUT_H_
+#define ENGINE_SRC_CORE_INPUT_H_
+
+#include <utility>
+
+#include "core/Core.h"
+
+namespace engine {
+
+class ENGINE_API Input {
+ public:
+  // -------------------------------- Key input --------------------------------
+
+  inline static bool IsKeyPressed(int key_code)
+      { return kInput_->IsKeyPressedImpl(key_code); }
+
+  // ------------------------------- Mouse input -------------------------------
+
+  inline static float GetMouseX() { return kInput_->GetMouseXImpl(); }
+  inline static float GetMouseY() { return kInput_->GetMouseYImpl(); }
+  inline static std::pair<float, float> GetMousePosition() {
+      return kInput_->GetMousePositionImpl(); }
+  inline static bool IsMouseButtonPressed(int button) {
+      return kInput_->IsMouseButtonPressedImpl(button); }
+
+ protected:
+  virtual bool IsKeyPressedImpl(int key_code) = 0;
+
+  virtual float GetMouseXImpl() = 0;
+  virtual float GetMouseYImpl() = 0;
+  virtual std::pair<float, float> GetMousePositionImpl() = 0;
+  virtual bool IsMouseButtonPressedImpl(int button) = 0;
+
+ private:
+  static Input* kInput_;
+};
+
+}  // namespace engine
+
+#endif  // ENGINE_SRC_CORE_INPUT_H_
