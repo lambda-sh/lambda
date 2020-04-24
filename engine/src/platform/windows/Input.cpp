ccadf02492488a5305d4203ff43034b5ce95648f
C3NZ <cenz@cenz.io>
2020-04-24 10:21:10 -0700

    [add] windows implementation for Input with glfw on windows platforms. It's exactly like linuxes for now as GLFW handles differentation for keys so far.

---
+++ engine/src/platform/windows/Input.cpp
@@ -0,0 +1,67 @@
+#if defined ENGINE_PLATFORM_WINDOWS || defined ENGINE_DEBUG
+
+// TODO(C3NZ): Implement this for the windows platform
+
+#include "platform/windows/Input.h"
+
+#include <utility>
+
+#include <GLFW/glfw3.h>
+
+#include "core/Application.h"
+#include "core/Input.h"
+
+namespace engine {
+
+#ifdef ENGINE_PLATFORM_WINDOWS
+
+Input* Input::kInput_ = new platform::windows::InputImplementation();
+
+#endif  // ENGINE_PLATFORM_WINDOWS
+
+namespace platform {
+namespace windows {
+
+
+bool InputImplementation::IsKeyPressedImpl(int key_code) {
+  GLFWwindow* window = static_cast<GLFWwindow*>(
+      Application::GetApplication().GetWindow().GetNativeWindow());
+
+  int state = glfwGetKey(window, key_code);
+  return state == GLFW_PRESS || state == GLFW_REPEAT;
+}
+
+float InputImplementation::GetMouseXImpl() {
+  std::pair<float, float> position = GetMousePositionImpl();
+  return position.first;
+}
+
+float InputImplementation::GetMouseYImpl() {
+  std::pair<float, float> position = GetMousePositionImpl();
+  return position.second;
+}
+
+std::pair<float, float> InputImplementation::GetMousePositionImpl() {
+  GLFWwindow* window = static_cast<GLFWwindow*>(
+      Application::GetApplication().GetWindow().GetNativeWindow());
+
+  double x_pos, y_pos;
+  glfwGetCursorPos(window, &x_pos, &y_pos);
+  return std::make_pair(
+      static_cast<float>(x_pos), static_cast<float>(y_pos));
+}
+
+bool InputImplementation::IsMouseButtonPressedImpl(int button) {
+  GLFWwindow* window = static_cast<GLFWwindow*>(
+      Application::GetApplication().GetWindow().GetNativeWindow());
+
+
+  int state = glfwGetMouseButton(window, button);
+  return state == GLFW_PRESS;
+}
+
+}  // namespace windows
+}  // namespace platform
+}  // namespace engine
+
+#endif  // ENGINE_PLATFORM_WINDOWS
---
+++ engine/src/platform/windows/Input.h
@@ -0,0 +1,32 @@
+#ifndef ENGINE_SRC_PLATFORM_WINDOWS_INPUT_H_
+#define ENGINE_SRC_PLATFORM_WINDOWS_INPUT_H_
+
+#if defined ENGINE_PLATFORM_WINDOWS || defined ENGINE_DEBUG
+
+#include <utility>
+
+#include <GLFW/glfw3.h>
+
+#include "core/Input.h"
+
+// TODO(C3NZ): Implement this for the windows platform
+namespace engine {
+namespace platform {
+namespace windows {
+
+class InputImplementation : public Input {
+ protected:
+  bool IsKeyPressedImpl(int key_code) override;
+
+  float GetMouseXImpl() override;
+  float GetMouseYImpl() override;
+  std::pair<float, float> GetMousePositionImpl() override;
+  bool IsMouseButtonPressedImpl(int button) override;
+};
+
+}  // namespace windows
+}  // namespace platform
+}  // namespace engine
+
+#endif  // ENGINE_PLATFORM_WINDOWS
+#endif  // ENGINE_SRC_PLATFORM_WINDOWS_INPUT_H_
