6c52b70af859a040acff354733a6a139ff6b39fe
C3NZ <cenz@cenz.io>
2020-04-23 20:39:55 -0700

    [add] the linux implementation for basic input capture with glfw.

---
+++ engine/src/platform/linux/Input.cpp
@@ -0,0 +1,67 @@
+#if defined ENGINE_PLATFORM_LINUX || defined ENGINE_DEBUG
+
+// TODO(C3NZ): Implement this for the windows platform
+
+#include "platform/linux/Input.h"
+
+#include <utility>
+
+#include <GLFW/glfw3.h>
+
+#include "core/Application.h"
+#include "core/Input.h"
+
+namespace engine {
+
+#ifdef ENGINE_PLATFORM_LINUX
+
+Input* Input::kInput_ = new platform::linux::InputImplementation();
+
+#endif  // ENGINE_PLATFORM_LINUX
+
+namespace platform {
+namespace linux {
+
+
+bool InputImplementation::IsKeyPressedImpl(int key_code) {
+  GLFWwindow* window = static_cast<GLFWwindow*>(
+      Application::GetApplication().GetWindow().GetNativeWindow());
+
+  int state = glfwGetKey(window, key_code);
+  return state == GLFW_PRESS || state == GLFW_REPEAT;
+}
+
+float InputImplementation::GetMouseXImpl() {
+  std::pair<float, float> position = GetMousePositionImpl();
+  return position.first;
+}
+
+float InputImplementation::GetMouseYImpl() {
+  std::pair<float, float> position = GetMousePositionImpl();
+  return position.second;
+}
+
+std::pair<float, float> InputImplementation::GetMousePositionImpl() {
+  GLFWwindow* window = static_cast<GLFWwindow*>(
+      Application::GetApplication().GetWindow().GetNativeWindow());
+
+  double x_pos, y_pos;
+  glfwGetCursorPos(window, &x_pos, &y_pos);
+  return std::make_pair(
+      static_cast<float>(x_pos), static_cast<float>(y_pos));
+}
+
+bool InputImplementation::IsMouseButtonPressedImpl(int button) {
+  GLFWwindow* window = static_cast<GLFWwindow*>(
+      Application::GetApplication().GetWindow().GetNativeWindow());
+
+
+  int state = glfwGetMouseButton(window, button);
+  return state == GLFW_PRESS;
+}
+
+}  // namespace linux
+}  // namespace platform
+}  // namespace engine
+
+#endif  // ENGINE_PLATFORM_LINUX
---
+++ engine/src/platform/linux/Input.h
@@ -0,0 +1,34 @@
+#ifndef ENGINE_SRC_PLATFORM_LINUX_INPUT_H_
+#define ENGINE_SRC_PLATFORM_LINUX_INPUT_H_
+
+#if defined ENGINE_PLATFORM_LINUX || defined ENGINE_DEBUG
+
+#include <utility>
+
+#include <GLFW/glfw3.h>
+
+#include "core/Input.h"
+
+// TODO(C3NZ): Implement this for the windows platform
+namespace engine {
+namespace platform {
+namespace linux {
+
+class InputImplementation : public Input {
+ protected:
+  bool IsKeyPressedImpl(int key_code) override;
+
+  float GetMouseXImpl() override;
+  float GetMouseYImpl() override;
+  std::pair<float, float> GetMousePositionImpl() override;
+  bool IsMouseButtonPressedImpl(int button) override;
+};
+
+}  // namespace linux
+}  // namespace platform
+}  // namespace engine
+
+
+
+#endif  // ENGINE_PLATFORM_LINUX
+#endif  // ENGINE_SRC_PLATFORM_LINUX_INPUT_H_
