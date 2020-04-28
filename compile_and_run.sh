63ab2b2923523a2b2cf101bc35769ec2a5188a09
C3NZ <cenz@cenz.io>
2020-04-27 19:12:30 -0700

    [add] -j to run more jobs for make, resulting in faster builds.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -2,10 +2,10 @@ export CC=gcc CXX=g++
 
 if [ "$1" = "Release" ] || [ "$1" = "Debug" ]; then
     echo "Compiling a $1 build."
-    cmake -DCMAKE_BUILD_TYPE="$1" \
+    cmake \
+        -DCMAKE_BUILD_TYPE="$1" \
         -DDISTRIBUTION_BUILD=False \
-        -DENGINE_DEVELOPMENT_MODE=True \
-        .
+        -DENGINE_DEVELOPMENT_MODE=True .
 elif [ "$1" = "Dist" ]; then
     echo "Compiling a Distribution build."
     cmake -DCMAKE_BUILD_TYPE="Release" -DDISTRIBUTION_BUILD=True .
@@ -14,5 +14,5 @@ else
     exit
 fi
 
-make
+make -j 8
 ./"$1"/bin/app
