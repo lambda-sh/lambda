c14d710487c230b21b180b7f59b510e86a46b35d
C3NZ <cenz@cenz.io>
2020-05-10 18:59:50 -0700

    [fix] the shader color.

--- engine/src/core/Application.cpp
+++ engine/src/core/Application.cpp
@@ -76,7 +76,7 @@ Application::Application() {
       layout(location = 0) out vec4 color;
 
       void main() {
-        color = vec4(1.0, 0.2, 0.3, 1.0);
+        color = vec4(0.3, 0.5, 0.3, 1.0);
       }
   )";
 
