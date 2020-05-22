aa23adcf1400a68ac19322c61d9c2fdce1be7085
C3NZ <cenz@cenz.io>
2020-05-21 23:42:29 -0700

    [add] the index buffer implementation and also setup a basic generalized Vertex/Index buffer implementation for clients.

--- engine/src/core/renderer/Buffer.cpp
+++ engine/src/core/renderer/Buffer.cpp
@@ -1,7 +1,43 @@
 #include "core/renderer/Buffer.h"
 
+#include "core/Assert.h"
+#include "core/renderer/Renderer.h"
+#include "platform/opengl/OpenGLBuffer.h"
+
 namespace engine {
 namespace renderer {
+
+VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size) {
+  switch (Renderer::GetAPI()) {
+    case RendererAPI::None:
+      ENGINE_CORE_ASSERT(
+          false, "There is no rendering API being used/available.");
+      return nullptr;
+    case RendererAPI::OpenGL:
+      return new platform::opengl::OpenGLVertexBuffer(vertices, size);
+    default:
+      ENGINE_CORE_ASSERT(
+          false,
+          "The Renderer has been set to a graphics API that isn't supported.");
+      return nullptr;
+  }
+}
+
+IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t count) {
+  switch (Renderer::GetAPI()) {
+    case RendererAPI::None:
+      ENGINE_CORE_ASSERT(
+          false, "There is no rendering API being used/available.");
+      return nullptr;
+    case RendererAPI::OpenGL:
+      return new platform::opengl::OpenGLIndexBuffer(indices, count);
+    default:
+      ENGINE_CORE_ASSERT(
+          false,
+          "The Renderer has been set to a graphics API that isn't supported.");
+      return nullptr;
+  }
+}
 
 }  // namespace renderer
 }  // namespace engine
--- engine/src/core/renderer/Buffer.h
+++ engine/src/core/renderer/Buffer.h
@@ -1,6 +1,8 @@
 #ifndef ENGINE_SRC_CORE_RENDERER_BUFFER_H_
 #define ENGINE_SRC_CORE_RENDERER_BUFFER_H_
 
+#include <bits/stdint-uintn.h>
+
 namespace engine {
 namespace renderer {
 
@@ -8,13 +10,22 @@ class VertexBuffer {
  public:
   virtual ~VertexBuffer() {}
 
-  virtual void Bind() = 0;
-  virtual void Unbind() = 0;
+  virtual void Bind() const = 0;
+  virtual void Unbind() const = 0;
+
+  static VertexBuffer* Create(float* vertices, uint32_t size);
 };
 
 class IndexBuffer {
  public:
   virtual ~IndexBuffer() {}
+
+  virtual void Bind() const = 0;
+  virtual void Unbind() const = 0;
+
+  virtual uint32_t GetCount() const = 0;
+
+  static IndexBuffer* Create(uint32_t* indices, uint32_t count);
 };
 
 }  // namespace renderer
