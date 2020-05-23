1ffedac42b2c3153a9f451fbaecbe0c6615f4699
C3NZ <cenz@cenz.io>
2020-05-22 22:16:08 -0700

    [test] out the applications new special styled string.

--- docs/menudata.js
+++ docs/menudata.js
@@ -1,4 +1,2 @@
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"Main Page",url:"index.html"}]}
