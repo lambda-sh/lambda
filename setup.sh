dd495823251837499fc218c15780c5fd7a2bc39a
C3NZ <cenz@cenz.io>
2020-05-26 12:27:02 -0700

    [add] a safety check to ensure that the script is only ever operable from within the root of the game engine.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+pushd $ROOT_DIR
+
 # Install pre-commit configs for working on the project.
 pre-commit install
 
 # Load git submodules.
 git submodule update --init
+
+popd  # ROOT_DIR
