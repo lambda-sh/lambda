d3f31a9dda167b3f554fb67fb433c6e225b73101
C3NZ <cenz@cenz.io>
2020-05-30 14:53:15 -0700

    [add] documentation about the engine rendering system.

--- engine/src/core/renderer/Renderer.h
+++ engine/src/core/renderer/Renderer.h
@@ -1,3 +1,7 @@
+/**
+ * @file engine/src/core/renderer/Renderer.h
+ * @brief The rendering API.
+ */
 #ifndef ENGINE_SRC_CORE_RENDERER_RENDERER_H_
 #define ENGINE_SRC_CORE_RENDERER_RENDERER_H_
 
@@ -5,8 +9,11 @@ namespace engine {
 namespace renderer {
 
 /**
+ * @enum RendererAPI
+ * @brief The graphics APIs that are available through the engine.
+ *
  * Available rendering APIs to be set in games for the rendering engine to set
- * which graphics context to use at runtime.
+ * which graphics context to use at runtime. Currently only supports OpenGL.
  */
 enum class RendererAPI {
   None = 0,
@@ -14,6 +21,10 @@ enum class RendererAPI {
 };
 
 /**
+ * @class Renderer
+ * @brief A lightweight rendering API implementation. Allows generalized calls
+ * to be written for users
+ *
  * A lightweight and not fully finished rendering API that lets you set the a
  * specific graphics context to use for rendering. This must be set externally
  * in any rendering application.
--- engine/src/core/renderer/Shader.h
+++ engine/src/core/renderer/Shader.h
@@ -1,3 +1,7 @@
+/**
+ * @file engine/src/core/renderer/Shader.h
+ * @brief Shader API to be used with the renderer.
+ */
 #ifndef ENGINE_SRC_CORE_RENDERER_SHADER_H_
 #define ENGINE_SRC_CORE_RENDERER_SHADER_H_
 
@@ -6,13 +10,37 @@
 namespace engine {
 namespace renderer {
 
+/**
+ * @class Shader
+ * @brief The Shader API.
+ *
+ * Expects sources to be strings that are designed for the shading language
+ * that is being used in the API.
+ */
 class Shader {
  public:
+  /**
+   * @fn Shader
+   * @brief Instantiates a Shader with vertex and fragment source strings.
+   *
+   * Source strings are expected to be written for the shading language that
+   * the current graphics API is using to compile shaders.
+   */
   Shader(const std::string& vertexSource, const std::string& fragmentSource);
   ~Shader();
 
+  /**
+   * @fn Bind
+   * @brief Binds the shader to the current graphics context.
+   */
   void Bind() const;
+
+  /**
+   * @fn Unbind
+   * @brief Unbinds the shader from the current graphics context.
+   */
   void Unbind() const;
+
  private:
   std::uint32_t renderer_ID_;
 };
