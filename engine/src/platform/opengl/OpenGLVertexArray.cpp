44ccf71317121bf3bac9ed00afdc0b223f7f6c20
C3NZ <cenz@cenz.io>
2020-06-04 15:13:43 -0700

    [add] the OpenGL specific implementation for Vertex Arrays.

---
+++ engine/src/platform/opengl/OpenGLVertexArray.cpp
@@ -0,0 +1,81 @@
+#include "platform/opengl/OpenGLVertexArray.h"
+
+#include <glad/glad.h>
+
+#include "core/Assert.h"
+#include "core/renderer/Buffer.h"
+
+namespace engine {
+namespace platform {
+namespace opengl {
+
+/**
+ * @fn ShaderDataTypeToOpenGLBaseType
+ * @brief Convert an renderer shader type to it's corresponding OpenGL type.
+ */
+static GLenum ShaderDataTypeToOpenGLBaseType(
+    engine::renderer::ShaderDataType type) {
+  switch (type) {
+    case engine::renderer::ShaderDataType::Bool: return GL_BOOL;
+    case engine::renderer::ShaderDataType::Float2: return GL_FLOAT;
+    case engine::renderer::ShaderDataType::Float3: return GL_FLOAT;
+    case engine::renderer::ShaderDataType::Float4: return GL_FLOAT;
+    case engine::renderer::ShaderDataType::Float: return GL_FLOAT;
+    case engine::renderer::ShaderDataType::Int2: return GL_INT;
+    case engine::renderer::ShaderDataType::Int3: return GL_INT;
+    case engine::renderer::ShaderDataType::Int4: return GL_INT;
+    case engine::renderer::ShaderDataType::Int: return GL_INT ;
+    case engine::renderer::ShaderDataType::Mat3: return GL_FLOAT;
+    case engine::renderer::ShaderDataType::Mat4: return GL_FLOAT;
+    default: ENGINE_CORE_ASSERT(false, "Unknown shader type."); return 0;
+  }
+}
+
+OpenGLVertexArray::OpenGLVertexArray() {
+  glCreateVertexArrays(1, &renderer_id_);
+}
+
+void OpenGLVertexArray::Bind() const {
+  glBindVertexArray(renderer_id_);
+}
+
+void OpenGLVertexArray::Unbind() const {
+  glBindVertexArray(0);
+}
+
+void OpenGLVertexArray::AddVertexBuffer(
+    const std::shared_ptr<renderer::VertexBuffer>& vertex_buffer) {
+  glBindVertexArray(renderer_id_);
+  vertex_buffer->Bind();
+
+
+  uint32_t index = 0;
+  const engine::renderer::BufferLayout& layout = vertex_buffer->GetLayout();
+  ENGINE_CORE_ASSERT(layout.HasElements(), "The vertex buffer doesn't have a layout.");
+
+  for (const renderer::BufferElement& element : layout) {
+    glEnableVertexAttribArray(index);
+    glVertexAttribPointer(
+        index,
+        element.Components,
+        ShaderDataTypeToOpenGLBaseType(element.Type),
+        element.Normalized ? GL_TRUE : GL_FALSE,
+        layout.GetStride(),
+        reinterpret_cast<const void*>(element.Offset));
+    ++index;
+  }
+
+  vertex_buffers_.push_back(vertex_buffer);
+}
+
+void OpenGLVertexArray::SetIndexBuffer(
+    const std::shared_ptr<renderer::IndexBuffer>& index_buffer) {
+  glBindVertexArray(renderer_id_);
+  index_buffer->Bind();
+
+  index_buffer_ = index_buffer;
+}
+
+}  // namespace opengl
+}  // namespace platform
+}  // namespace engine
---
+++ engine/src/platform/opengl/OpenGLVertexArray.h
@@ -0,0 +1,64 @@
+#ifndef ENGINE_SRC_PLATFORM_OPENGL_OPENGLVERTEXARRAY_H_
+#define ENGINE_SRC_PLATFORM_OPENGL_OPENGLVERTEXARRAY_H_
+
+#include <bits/stdint-uintn.h>
+#include <vector>
+
+#include "core/renderer/Buffer.h"
+#include "core/renderer/VertexArray.h"
+
+namespace engine {
+namespace platform {
+namespace opengl {
+
+/**
+ * @class VertexArray
+ * @brief The abstraction for representing Vertex arrays and their sub
+ * components.
+ */
+class OpenGLVertexArray : public renderer::VertexArray {
+ public:
+  OpenGLVertexArray();
+  ~OpenGLVertexArray() {}
+
+  /**
+   * @fn Bind
+   * @brief Bind the Vertex array and it's components to the rendering API and
+   * GPU.
+   */
+  void Bind() const override;
+
+  /**
+   * @fn Unbind
+   * @brief Unbind the vertex array and it's components from the rendering API
+   * and memory.
+   */
+  void Unbind() const override;
+
+  /**
+   * @fn AddVertexBuffer
+   * @brief Add a vertex buffer to the current Vertex Array.
+   */
+  void AddVertexBuffer(
+      const std::shared_ptr<renderer::VertexBuffer>& vertex_buffer) override;
+
+  /**
+   * @fn SetIndexBuffer
+   * @brief Set the index buffer for rendering all vertex arrays.
+   */
+  void SetIndexBuffer(
+      const std::shared_ptr<renderer::IndexBuffer>& index_buffer) override;
+
+ private:
+  std::shared_ptr<renderer::IndexBuffer> index_buffer_;
+  std::vector<std::shared_ptr<renderer::VertexBuffer>> vertex_buffers_;
+  uint32_t renderer_id_;
+
+};
+
+
+}  // namespace opengl
+}  // namespace platform
+}  // namespace engine
+
+#endif  // ENGINE_SRC_PLATFORM_OPENGL_OPENGLVERTEXARRAY_H_
