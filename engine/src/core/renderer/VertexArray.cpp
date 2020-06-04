8c0f14601143f92f371699c595ce5eb95a1a95be
C3NZ <cenz@cenz.io>
2020-06-04 15:12:59 -0700

    [add] the Renderer Vertex Array abstraction that can be implemented by platform specific graphic APIs. Also add const iterators to the Buffer layout that allows elements to accessed by read only iterators.

---
+++ engine/src/core/renderer/VertexArray.cpp
@@ -0,0 +1,31 @@
+#include "core/renderer/VertexArray.h"
+
+#include "core/Assert.h"
+#include "core/renderer/Renderer.h"
+
+#include "platform/opengl/OpenGLVertexArray.h"
+
+namespace engine {
+namespace renderer {
+
+/**
+ * Only supports Vertex buffers that are available through the rendering API.
+ */
+VertexArray* VertexArray::Create() {
+  switch (Renderer::GetAPI()) {
+    case RendererAPI::None:
+      ENGINE_CORE_ASSERT(
+          false, "There is no rendering API being used/available.");
+      return nullptr;
+    case RendererAPI::OpenGL:
+      return new engine::platform::opengl::OpenGLVertexArray();
+    default:
+      ENGINE_CORE_ASSERT(
+          false,
+          "The Renderer has been set to a graphics API that isn't supported.");
+      return nullptr;
+  }
+}
+
+}  // namespace renderer
+}  // namespace engine
