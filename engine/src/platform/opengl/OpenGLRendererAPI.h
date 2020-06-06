fbe0476b1f280e8660229127fe8c3f333d711715
C3NZ <cenz@cenz.io>
2020-06-06 15:13:58 -0700

    [add] the RendererAPI implementation for OpenGL. The resulting class implements the abstract RendererAPI to be compatible with the engine's rendering API.

---
+++ engine/src/platform/opengl/OpenGLRendererAPI.h
@@ -0,0 +1,34 @@
+/**
+ * @file engine/src/platform/opengl/OpenGLRendererAPI.h
+ * @brief The OpenGL rendering implementation.
+ */
+#ifndef ENGINE_SRC_PLATFORM_OPENGL_OPENGLRENDERERAPI_H_
+#define ENGINE_SRC_PLATFORM_OPENGL_OPENGLRENDERERAPI_H_
+
+#include "core/renderer/RendererAPI.h"
+#include "core/renderer/VertexArray.h"
+
+namespace engine {
+namespace platform {
+namespace opengl {
+
+/**
+ * @class OpenGLRendererAPI
+ * @brief The Rendering implementation for OpenGL.
+ */
+class OpenGLRendererAPI : public renderer::RendererAPI {
+ public:
+  void SetClearColor(const glm::vec4& color) override;
+  void Clear() override;
+
+  void DrawIndexed(const std::shared_ptr<renderer::VertexArray>& vertex_array)
+      override;
+
+};
+
+
+}  // namespace opengl
+}  // namespace platform
+}  // namespace engine
+
+#endif  // ENGINE_SRC_PLATFORM_OPENGL_OPENGLRENDERERAPI_H_
