46b35a14c7a60a57b14215eb6a666580eb44a7ca
C3NZ <cenz@cenz.io>
2020-06-13 18:20:07 -0700

    [update] the project to use c++20 as the version standard and implement a concept into the Reverse class to enforce the use of iterables with Reverse.

--- engine/src/core/util/Reverse.h
+++ engine/src/core/util/Reverse.h
@@ -5,15 +5,20 @@
 #ifndef ENGINE_SRC_CORE_UTIL_REVERSE_H_
 #define ENGINE_SRC_CORE_UTIL_REVERSE_H_
 
+#include <ranges>
+
 namespace engine {
 namespace util {
+
+template<class Container>
+concept Iterable = std::ranges::bidirectional_range<Container>;
 
 /**
  * @class Reverse
  * @brief Provides a clean interface for iterating through any container that
  * implements rbegin and rend.
  */
-template<class Container>
+template<Iterable Container>
 class Reverse {
  public:
   explicit Reverse(Container& container) : container_(container) {}
