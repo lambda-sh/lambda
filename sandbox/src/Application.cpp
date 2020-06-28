6a8d271e0cc111e84d96c7ae348917dd91956a6e
C3NZ <cenz@cenz.io>
2020-06-28 16:49:23 -0700

    [add] a working example of using the transform system.

--- sandbox/src/Application.cpp
+++ sandbox/src/Application.cpp
@@ -1,13 +1,15 @@
 #include <glm/glm.hpp>
 
 #include "Engine.h"
+#include "ext/matrix_transform.hpp"
 
 class ExampleLayer : public engine::Layer {
  public:
   ExampleLayer() :
       Layer("Example"),
       camera_(-1.6f, 1.6f, -0.9f, 0.9f),
-      camera_position_({0.0f, 0.0f, 0.0f}) {
+      camera_position_({0.0f, 0.0f, 0.0f}),
+      square_position_(0.0f) {
     // Setup our vertices.
     float vertices[3 * 7] = {
       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.9f, 1.0f,
@@ -86,14 +88,28 @@ class ExampleLayer : public engine::Layer {
       camera_position_.x += camera_speed_ * ts;
     }
 
+    if (engine::Input::IsKeyPressed(ENGINE_KEY_I)) {
+      square_position_.y += square_move_speed_ * ts;
+    } else if (engine::Input::IsKeyPressed(ENGINE_KEY_K)) {
+      square_position_.y -= square_move_speed_ * ts;
+    }
+
+    if (engine::Input::IsKeyPressed(ENGINE_KEY_J)) {
+      square_position_.x -= camera_speed_ * ts;
+    } else if (engine::Input::IsKeyPressed(ENGINE_KEY_L)) {
+      square_position_.x += camera_speed_ * ts;
+    }
+
     engine::renderer::RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1.0f});
     engine::renderer::RenderCommand::Clear();
 
     camera_.SetPosition(camera_position_);
     camera_.SetRotation(45.0f);
 
+    glm::mat4 transform = glm::translate(glm::mat4(1.0f), square_position_);
+
     engine::renderer::Renderer::BeginScene(camera_);
-    engine::renderer::Renderer::Submit(vertex_array_, shader_);
+    engine::renderer::Renderer::Submit(vertex_array_, shader_, transform);
     engine::renderer::Renderer::EndScene();
   }
 
@@ -109,15 +125,14 @@ class ExampleLayer : public engine::Layer {
 
   engine::renderer::OrthographicCamera camera_;
   glm::vec3 camera_position_;
+  glm::vec3 square_position_;
   float camera_speed_ = 0.01f;
+  float square_move_speed_ = 0.03f;
 };
 
 class Sandbox : public engine::Application {
  public:
-  Sandbox() {
-    PushLayer(new ExampleLayer());
-  }
-
+  Sandbox() { PushLayer(new ExampleLayer()); }
   ~Sandbox() {}
 };
 
