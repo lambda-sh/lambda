2b54e93635d82abebea8fbed8616a9fe169a3a64
C3NZ <cenz@cenz.io>
2020-06-28 15:09:40 -0700

    [add] the a transformation matrix to the shader.

--- sandbox/src/Application.cpp
+++ sandbox/src/Application.cpp
@@ -41,6 +41,7 @@ class ExampleLayer : public engine::Layer {
         layout(location = 1) in vec4 a_Color;
 
         uniform mat4 u_ViewProjection;
+        uniform mat4 u_Transform;
 
         out vec3 v_Position;
         out vec4 v_Color;
@@ -48,7 +49,7 @@ class ExampleLayer : public engine::Layer {
         void main() {
           v_Position = a_Position;
           v_Color = a_Color;
-          gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+          gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
         }
     )";
 
