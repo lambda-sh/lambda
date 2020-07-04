cd24b5f22dd6a88b362aa62bee02e5c57abe6c04
C3NZ <cenz@cenz.io>
2020-07-04 15:42:00 -0700

    [add] platform specific functions for uploading data into the shader.

--- engine/src/platform/opengl/OpenGLShader.cpp
+++ engine/src/platform/opengl/OpenGLShader.cpp
@@ -1,4 +1,4 @@
-#include "platform/opengl/OpenGLOpenGLShader.h"
+#include "platform/opengl/OpenGLShader.h"
 
 #include <string>
 #include <vector>
@@ -13,42 +13,42 @@ namespace platform {
 namespace opengl {
 
 
-OpenGLOpenGLShader::OpenGLShader(
+OpenGLShader::OpenGLShader(
     const std::string& vertex_source, const std::string& fragment_source) {
-  unsigned int vertex_shader = glCreateOpenGLShader(GL_VERTEX_SHADER);
+  unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
   const char* vertex_program = vertex_source.c_str();
 
   int has_compiled = GL_FALSE;
-  glOpenGLShaderSource(vertex_shader, 1, &vertex_program, 0);
-  glCompileOpenGLShader(vertex_shader);
-  glGetOpenGLShaderiv(vertex_shader, GL_COMPILE_STATUS, &has_compiled);
+  glShaderSource(vertex_shader, 1, &vertex_program, 0);
+  glCompileShader(vertex_shader);
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &has_compiled);
 
   if (has_compiled == GL_FALSE) {
     int maxLength = 0;
-    glGetOpenGLShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &maxLength);
+    glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &maxLength);
     std::vector<char> info_log(maxLength);
-    glGetOpenGLShaderInfoLog(vertex_shader, maxLength, &maxLength, &info_log[0]);
+    glGetShaderInfoLog(vertex_shader, maxLength, &maxLength, &info_log[0]);
 
-    glDeleteOpenGLShader(vertex_shader);
+    glDeleteShader(vertex_shader);
     ENGINE_CORE_ERROR(
         "Vertex shader compilation failure: {0}", info_log.data());
   }
 
-  uint32_t fragment_shader = glCreateOpenGLShader(GL_FRAGMENT_SHADER);
+  uint32_t fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
   const char* fragment_program = fragment_source.c_str();
 
-  glOpenGLShaderSource(fragment_shader, 1, &fragment_program, 0);
-  glCompileOpenGLShader(fragment_shader);
-  glGetOpenGLShaderiv(fragment_shader, GL_COMPILE_STATUS, &has_compiled);
+  glShaderSource(fragment_shader, 1, &fragment_program, 0);
+  glCompileShader(fragment_shader);
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &has_compiled);
 
   if (has_compiled == GL_FALSE) {
     int maxLength = 0;
-    glGetOpenGLShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &maxLength);
+    glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &maxLength);
     std::vector<char> info_log(maxLength);
-    glGetOpenGLShaderInfoLog(fragment_shader, maxLength, &maxLength, &info_log[0]);
+    glGetShaderInfoLog(fragment_shader, maxLength, &maxLength, &info_log[0]);
 
-    glDeleteOpenGLShader(fragment_shader);
-    glDeleteOpenGLShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    glDeleteShader(vertex_shader);
 
     ENGINE_CORE_ERROR(
         "Fragment shader compilation failure: {0}", info_log.data());
@@ -56,8 +56,8 @@ OpenGLOpenGLShader::OpenGLShader(
 
   renderer_ID_ = glCreateProgram();
 
-  glAttachOpenGLShader(renderer_ID_, vertex_shader);
-  glAttachOpenGLShader(renderer_ID_, fragment_shader);
+  glAttachShader(renderer_ID_, vertex_shader);
+  glAttachShader(renderer_ID_, fragment_shader);
   glLinkProgram(renderer_ID_);
 
   int program_linked = GL_FALSE;
@@ -71,17 +71,17 @@ OpenGLOpenGLShader::OpenGLShader(
     glGetProgramInfoLog(renderer_ID_, maxLength, &maxLength, &info_log[0]);
 
     glDeleteProgram(renderer_ID_);
-    glDeleteOpenGLShader(vertex_shader);
-    glDeleteOpenGLShader(fragment_shader);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
 
     ENGINE_CORE_ERROR("Linking failure: {0}", info_log.data());
   }
 
-  glDetachOpenGLShader(renderer_ID_, vertex_shader);
-  glDetachOpenGLShader(renderer_ID_, fragment_shader);
+  glDetachShader(renderer_ID_, vertex_shader);
+  glDetachShader(renderer_ID_, fragment_shader);
 }
 
-OpenGLOpenGLShader::~OpenGLShader() {
+OpenGLShader::~OpenGLShader() {
   glDeleteProgram(renderer_ID_);
 }
 
@@ -93,10 +93,40 @@ void OpenGLShader::Unbind() const {
   glUseProgram(0);
 }
 
+void OpenGLShader::UploadUniformInt(
+    const std::string& name, const int& value) {
+  GLint location = glGetUniformLocation(renderer_ID_, name.c_str());
+  glUniform1i(location, value);
+}
+
+void OpenGLShader::UploadUniformFloat(
+    const std::string& name, const float& value) {
+  GLint location = glGetUniformLocation(renderer_ID_, name.c_str());
+  glUniform1f(location, value);
+}
+
+void OpenGLShader::UploadUniformFloat2(
+    const std::string& name, const glm::vec2& values) {
+  GLint location = glGetUniformLocation(renderer_ID_, name.c_str());
+  glUniform2f(location, values.x, values.y);
+}
+
+void OpenGLShader::UploadUniformFloat3(
+    const std::string& name, const glm::vec3& values) {
+  GLint location = glGetUniformLocation(renderer_ID_, name.c_str());
+  glUniform3f(location, values.x, values.y, values.z);
+}
+
 void OpenGLShader::UploadUniformFloat4(
     const std::string& name, const glm::vec4& values) {
   GLint location = glGetUniformLocation(renderer_ID_, name.c_str());
   glUniform4f(location, values.x, values.y, values.z, values.a);
+}
+
+void OpenGLShader::UploadUniformMat3(
+    const std::string& name, const glm::mat3& matrix) {
+  uint32_t location = glGetUniformLocation(renderer_ID_, name.c_str());
+  glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
 }
 
 void OpenGLShader::UploadUniformMat4(
