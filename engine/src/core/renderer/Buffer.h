887455b5eb2278fdf5a8b5e83d5cf7361a80055c
C3NZ <cenz@cenz.io>
2020-07-11 15:29:26 -0700

    [fix] rectangle rule.

--- engine/src/core/renderer/Buffer.h
+++ engine/src/core/renderer/Buffer.h
@@ -160,6 +160,7 @@ class IndexBuffer {
 
   virtual void Bind() const = 0;
   virtual void Unbind() const = 0;
+
   virtual uint32_t GetCount() const = 0;
 
   static memory::Shared<IndexBuffer> Create(uint32_t* indices, uint32_t count);
