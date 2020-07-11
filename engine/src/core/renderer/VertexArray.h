3fd0c8412587f245f9597a0be8989cc04ce19cc7
C3NZ <cenz@cenz.io>
2020-07-11 13:26:51 -0700

    [update] the vertex array API to use the engines memory management system.

--- engine/src/core/renderer/VertexArray.h
+++ engine/src/core/renderer/VertexArray.h
@@ -10,6 +10,7 @@
 
 #include <memory>
 
+#include "core/memory/Pointers.h"
 #include "core/renderer/Buffer.h"
 
 namespace engine {
@@ -23,18 +24,18 @@ class VertexArray {
   virtual void Unbind() const = 0;
 
   virtual void AddVertexBuffer(
-      const std::shared_ptr<VertexBuffer>& vertex_buffer) = 0;
+      const memory::Shared<VertexBuffer>& vertex_buffer) = 0;
 
   virtual void SetIndexBuffer(
-      const std::shared_ptr<IndexBuffer>& index_buffer) = 0;
+      const memory::Shared<IndexBuffer>& index_buffer) = 0;
 
-  virtual const std::shared_ptr<IndexBuffer>&
+  virtual const memory::Shared<IndexBuffer>&
       GetIndexBuffer() const = 0;
 
-  virtual const std::vector<std::shared_ptr<VertexBuffer>>&
+  virtual const std::vector<memory::Shared<VertexBuffer>>&
       GetVertexBuffers() const = 0;
 
-  static VertexArray* Create();
+  static memory::Shared<VertexArray> Create();
 };
 
 }  // namespace renderer
