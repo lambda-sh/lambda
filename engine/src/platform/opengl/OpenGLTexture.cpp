649b67191eb51eb3be6390e95edb554a7a3d2f2a
C3NZ <cenz@cenz.io>
2020-07-22 13:18:37 -0700

    [update] the OpenGL texture to determine the pixel size and type upon loading the image into memory.

--- engine/src/platform/opengl/OpenGLTexture.cpp
+++ engine/src/platform/opengl/OpenGLTexture.cpp
@@ -21,15 +21,40 @@ OpenGLTexture2D::OpenGLTexture2D(const std::string& path) : path_(path) {
   width_ = width;
   height_ = height;
 
+  // Pixel size and type. Needed for allocating the correct amount of
+  // memory with OpenGL.
+  int size_format = 0, type_format = 0;
+  switch(channels) {
+    case 4:
+      size_format = GL_RGBA8;
+      type_format = GL_RGBA;
+      break;
+    case 3:
+      size_format = GL_RGB8;
+      type_format = GL_RGB;
+  }
+
+  ENGINE_CORE_ASSERT(
+      size_format && type_format, "Pixel format for {0} not supported.",
+      path_);
+
   // Create the texture and specify some meta information about it.
   glCreateTextures(GL_TEXTURE_2D, 1, &renderer_ID_);
-  glTextureStorage2D(renderer_ID_, 1, GL_RGB8, width_, height_);
+  glTextureStorage2D(renderer_ID_, 1, size_format, width_, height_);
 
   // Set the upscaling and downscaling functions to be linear.
   glTextureParameteri(renderer_ID_, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTextureParameteri(renderer_ID_, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTextureSubImage2D(
-      renderer_ID_, 0, 0, 0, width_, height_, GL_RGB, GL_UNSIGNED_BYTE, data);
+      renderer_ID_,
+      0,
+      0,
+      0,
+      width_,
+      height_,
+      type_format,
+      GL_UNSIGNED_BYTE,
+      data);
 
   // Free the memory of the image now that it has been loaded into the GPU.
   stbi_image_free(data);
