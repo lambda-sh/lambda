2159e180a755b5d4c407e77383076f7575ce9856
C3NZ <cenz@cenz.io>
2020-07-25 12:52:27 -0700

    [update] the texture shader to be loaded into the sandbox via an asset file.

--- sandbox/src/Application.cpp
+++ sandbox/src/Application.cpp
@@ -77,40 +77,8 @@ class ExampleLayer : public engine::layers::Layer {
 
     shader_ = engine::renderer::Shader::Create(vertex_source, fragment_source);
 
-    std::string texture_vertex_source = R"(
-        #version 330 core
-
-        layout(location = 0) in vec3 a_Position;
-        layout(location = 1) in vec2 a_TexCoord;
-
-        uniform mat4 u_ViewProjection;
-        uniform mat4 u_Transform;
-
-        out vec2 v_TexCoord;
-
-        void main() {
-          v_TexCoord = a_TexCoord;
-          gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-        }
-    )";
-
-    std::string texture_fragment_source = R"(
-        #version 330 core
-
-        layout(location = 0) out vec4 color;
-
-        in vec2 v_TexCoord;
-
-        uniform sampler2D u_Texture;
-
-        void main() {
-
-          color = texture(u_Texture, v_TexCoord);
-        }
-    )";
-
     texture_shader_ = engine::renderer::Shader::Create(
-        texture_vertex_source, texture_fragment_source);
+        "assets/shaders/Texture.glsl");
 
     texture_ = engine::renderer::Texture2D::Create(
         "assets/textures/checkboard.png");
