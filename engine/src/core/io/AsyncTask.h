f99e1fe6edb6494639b0e87e5d3e962f804b7668
C3NZ <cenz@cenz.io>
2020-07-30 10:25:12 -0700

    [add] the header file for the AsyncTask class. This will be broken up in the future.

---
+++ engine/src/core/io/AsyncTask.h
@@ -0,0 +1,96 @@
+#ifndef ENGINE_SRC_CORE_IO_ASYNCTASK_H_
+#define ENGINE_SRC_CORE_IO_ASYNCTASK_H_
+
+#include <bits/stdint-uintn.h>
+#include <functional>
+
+#include "core/memory/Pointers.h"
+#include "core/util/Time.h"
+
+namespace engine {
+namespace io {
+
+class AsyncTask;
+
+typedef std::function<bool()> EventFunc;
+typedef memory::Unique<AsyncTask> UniqueAsyncTask;
+
+enum class AsyncStatus {
+  None = 0,
+  Deferred,
+  Ready,
+  Expired
+};
+
+enum class AsyncResult {
+  None = 0,
+  Failure,
+  Success
+};
+
+class AsyncTask {
+ public:
+  AsyncTask(
+      EventFunc callback,
+      util::Time execute_at = util::Time(),
+      util::Time expires_at = util::Time().AddSeconds(5)) :
+          callback_(callback),
+          scheduled_at_(util::Time()),
+          execute_at_(execute_at),
+          expires_at_(expires_at) {}
+
+  AsyncTask(EventFunc callback, uint32_t interval_in_ms, bool should_repeat) :
+      callback_(callback),
+      should_repeat_(should_repeat),
+      interval_in_ms_(interval_in_ms) {
+        scheduled_at_ = util::Time();
+        execute_at_ = scheduled_at_.AddMilliseconds(interval_in_ms_);
+        expires_at_ = util::Time(execute_at_.AddSeconds(5));
+      }
+
+  bool ShouldRepeat() { return should_repeat_; }
+
+  AsyncResult Execute() {
+    if (callback_()) {
+      return AsyncResult::Success;
+    }
+    return AsyncResult::Failure;
+  }
+
+  AsyncStatus GetStatus() {
+    if (expires_at_.HasPassed()) {
+      return AsyncStatus::Expired;
+    }
+
+    if (execute_at_.HasPassed()) {
+      return AsyncStatus::Ready;
+    }
+
+    return AsyncStatus::Deferred;
+  }
+
+  const std::string& GetName() const { return name_; }
+  const uint32_t GetIntervalInMilliseconds() const { return interval_in_ms_; }
+
+  // Sets when to execute at.
+  void ExecuteAt(util::Time new_time) {
+    execute_at_ = new_time;
+  }
+
+  void ExecuteIn(uint32_t milliseconds) {
+    execute_at_ = execute_at_.AddMilliseconds(milliseconds);
+    expires_at_ = execute_at_.AddSeconds(5);
+  }
+
+ private:
+  std::string name_;
+  EventFunc callback_;
+  bool should_repeat_ = false;
+  uint32_t interval_in_ms_;
+  util::Time scheduled_at_, execute_at_, executed_at_, expires_at_;
+};
+
+}  // namespace io
+}  // namespace engine
+
+#endif  // ENGINE_SRC_CORE_IO_ASYNCTASK_H_
