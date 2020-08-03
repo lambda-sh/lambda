43f362b630f3cdf0077057d93613a19bebe8ad84
C3NZ <cenz@cenz.io>
2020-08-03 12:43:38 -0700

    [refactor] the async task and event loop to separate definitions from their implementations. Also drop inlines.

---
+++ engine/src/core/io/AsyncTask.cpp
@@ -0,0 +1,40 @@
+#include "core/io/AsyncTask.h"
+
+#include "core/util/Time.h"
+
+namespace engine {
+namespace io {
+
+using util::Time;
+using util::Milliseconds;
+
+// TODO(C3NZ): Callbacks should be made more generic. Is it possible to allow
+// values to escape the callback once it's been resolved/rejected?
+AsyncResult AsyncTask::Execute() {
+  if (callback_()) {
+    return AsyncResult::Success;
+  }
+  return AsyncResult::Failure;
+}
+
+AsyncStatus AsyncTask::GetStatus() {
+  if (expires_at_.HasPassed()) {
+    return AsyncStatus::Expired;
+  }
+
+  if (execute_at_.HasPassed()) {
+    return AsyncStatus::Ready;
+  }
+
+  return AsyncStatus::Deferred;
+}
+
+void AsyncTask::RescheduleTask(
+    Time new_execution_time, Time new_expiration_time) {
+  execute_at_ = new_execution_time;
+  expires_at_ = new_expiration_time;
+}
+
+
+}  // namespace io
+}  // namespace engine
