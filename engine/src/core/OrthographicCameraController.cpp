a65525a102ca9f1f04b323363b7d072a9ca8f140
C3NZ <cenz@cenz.io>
2020-08-06 14:47:45 -0700

    [add] an orthographic camera controller for automatically providing an interface for moving an orthographic camera with WASD.

---
+++ engine/src/core/OrthographicCameraController.cpp
@@ -0,0 +1,93 @@
+#include "core/OrthographicCameraController.h"
+
+#include "core/Input.h"
+#include "core/KeyCodes.h"
+#include "core/events/ApplicationEvent.h"
+#include "core/events/Event.h"
+#include "core/events/MouseEvent.h"
+#include "core/memory/Pointers.h"
+#include "core/renderer/OrthographicCamera.h"
+#include "core/util/Time.h"
+
+namespace engine {
+
+using engine::events::Event;
+using engine::events::EventDispatcher;
+using engine::events::MouseScrolledEvent;
+using engine::events::WindowResizeEvent;
+using memory::Shared;
+using util::TimeStep;
+
+OrthographicCameraController::OrthographicCameraController(
+    float aspect_ratio, bool can_rotate) :
+        aspect_ratio_(aspect_ratio),
+        camera_(
+            -aspect_ratio_ * zoom_level_,
+            aspect_ratio_ * zoom_level_,
+            -zoom_level_,
+            zoom_level_),
+        can_rotate_(can_rotate) {}
+
+void OrthographicCameraController::OnUpdate(TimeStep delta) {
+    float delta_in_ms = delta.InSeconds<float>();
+    if (Input::IsKeyPressed(ENGINE_KEY_W)) {
+      camera_position_.y += camera_translation_speed_ * delta_in_ms;
+    } else if (Input::IsKeyPressed(ENGINE_KEY_S)) {
+      camera_position_.y -= camera_translation_speed_ * delta_in_ms;
+    }
+
+    if (Input::IsKeyPressed(ENGINE_KEY_A)) {
+      camera_position_.x -= camera_translation_speed_ * delta_in_ms;
+    } else if (Input::IsKeyPressed(ENGINE_KEY_D)) {
+      camera_position_.x += camera_translation_speed_ * delta_in_ms;
+    }
+
+    camera_.SetPosition(camera_position_);
+
+    if (can_rotate_) {
+      if (Input::IsKeyPressed(ENGINE_KEY_Q)) {
+        camera_rotation_ -= camera_rotation_speed_  * delta_in_ms;
+      } else if (Input::IsKeyPressed(ENGINE_KEY_E)) {
+        camera_rotation_ += camera_rotation_speed_ * delta_in_ms;
+      }
+
+      camera_.SetRotation(camera_rotation_);
+    }
+}
+
+bool OrthographicCameraController::OnMouseScrolled(
+    const MouseScrolledEvent& event) {
+  zoom_level_ -= event.GetYOffset() * 0.20f;
+  camera_.SetProjectionMatrix(
+      -aspect_ratio_ * zoom_level_,
+      aspect_ratio_ * zoom_level_,
+      -zoom_level_,
+      zoom_level_);
+  return false;
+}
+
+bool OrthographicCameraController::OnWindowResize(
+    const WindowResizeEvent& event) {
+  aspect_ratio_ = static_cast<float>(event.GetWidth()) / static_cast<float>(
+      event.GetHeight());
+
+  camera_.SetProjectionMatrix(
+      -aspect_ratio_ * zoom_level_,
+      aspect_ratio_ * zoom_level_,
+      -zoom_level_,
+      zoom_level_);
+
+  return false;
+}
+
+void OrthographicCameraController::OnEvent(Shared<Event> event) {
+  EventDispatcher dispatcher(event);
+
+  dispatcher.Dispatch<WindowResizeEvent>(
+      BIND_EVENT_FN(OrthographicCameraController::OnWindowResize));
+
+  dispatcher.Dispatch<MouseScrolledEvent>(
+      BIND_EVENT_FN(OrthographicCameraController::OnMouseScrolled));
+}
+
+}  // namespace engine
---
+++ engine/src/core/OrthographicCameraController.h
@@ -0,0 +1,45 @@
+#ifndef ENGINE_SRC_CORE_ORTHOGRAPHICCAMERACONTROLLER_H_
+#define ENGINE_SRC_CORE_ORTHOGRAPHICCAMERACONTROLLER_H_
+
+#include "core/events/ApplicationEvent.h"
+#include "core/events/MouseEvent.h"
+#include "core/memory/Pointers.h"
+#include "core/renderer/OrthographicCamera.h"
+#include "core/util/Time.h"
+
+namespace engine {
+
+using engine::events::Event;
+using engine::events::MouseScrolledEvent;
+using engine::events::WindowResizeEvent;
+using engine::memory::Shared;
+using engine::renderer::OrthographicCamera;
+using engine::util::TimeStep;
+
+class OrthographicCameraController {
+ public:
+  explicit OrthographicCameraController(
+      float aspect_ratio, bool can_rotate = false);
+
+  void OnUpdate(TimeStep delta);
+  void OnEvent(Shared<Event> event);
+
+  const OrthographicCamera& GetOrthographicCamera() { return camera_; }
+ private:
+  float aspect_ratio_;
+  bool can_rotate_;
+  float zoom_level_ = 1.0f;
+
+  renderer::OrthographicCamera camera_;
+  glm::vec3 camera_position_ = {0.0f, 0.0f, 0.0f};
+  float camera_translation_speed_ = 0.01f;
+  float camera_rotation_speed_ =  0.03f;
+  float camera_rotation_ = 0.0f;
+
+  bool OnMouseScrolled(const MouseScrolledEvent& event);
+  bool OnWindowResize(const WindowResizeEvent& event);
+};
+
+}  // namespace engine
+
+#endif  // ENGINE_SRC_CORE_ORTHOGRAPHICCAMERACONTROLLER_H_
