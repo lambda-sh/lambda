37407bdf51844f0fa62a82626fb8b45386ab2f78
C3NZ <cenz@cenz.io>
2020-08-11 19:58:19 -0700

    [update] documentation and also make the Event Dispatcher more readable, since it wasn't clear as to what was going on before.

--- lambda/src/core/events/Event.h
+++ lambda/src/core/events/Event.h
@@ -1,12 +1,10 @@
-/**
- * @file Event.h
- * @brief The Event class and dispatcher core implementations.
- *
- * The event system is a core component of the game engine that enables the
- * engine to act upon user input by propagating the user input as event
- * across layers that are attached to the engines layer stack. This enables
- * events to be passed to prioritized layers. (More specifically, overlays)
- */
+/// @file Event.h
+/// @brief The Event class and dispatcher core implementations.
+///
+/// The event system is a core component of the game engine that enables the
+/// engine to act upon user input by propagating the user input as event
+/// across layers that are attached to the engines layer stack. This enables
+/// events to be passed to prioritized layers. (More specifically, overlays)
 #ifndef LAMBDA_SRC_CORE_EVENTS_EVENT_H_
 #define LAMBDA_SRC_CORE_EVENTS_EVENT_H_
 
@@ -25,6 +23,7 @@ namespace events {
 
 // ----------------------- EVENT TYPES & CATEGORIES ---------------------------
 
+/// @brief Event types natively supported by lambda.
 enum class EventType {
   None = 0,
   kWindowClose, kWindowResize, kWindowFocus, kWindowLostFocus, kWindowMoved,
@@ -33,6 +32,7 @@ enum class EventType {
   kMouseButtonPressed, kMouseButtonReleased, kMouseMoved, kMouseScrolled
 };
 
+/// @brief Event categories natively supported by lambda.
 enum EventCategory {
   None = 0,
   kEventCategoryApplication = BIT(0),
@@ -44,18 +44,24 @@ enum EventCategory {
 
 // ------------------------------------- MACROS --------------------------------
 
+/// @brief Utility macro to make events compatible with lambdas EventDispatcher.
 #define EVENT_CLASS_TYPE(type) \
     static EventType GetStaticType() { return EventType::type; } \
     EventType GetEventType() const override { return GetStaticType(); } \
     const char* GetName() const override { return #type; }
 
+/// @brief Utility macro to make events compatible with lambdas EventDispacher.
 #define EVENT_CLASS_CATEGORY(category) \
     int GetCategoryFlags() const override { return category; }
 
+/// @brief Utility function used for binding functions to lambdas
+/// EventDispatcher.
 #define BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
 
 // ----------------------------------- CLASSES ---------------------------------
 
+/// @brief The base Event class for events that are propagated throughout
+/// lambda.
 class Event {
   friend class EventDispatcher;
  public:
@@ -64,19 +70,23 @@ class Event {
   virtual int GetCategoryFlags() const = 0;
   virtual std::string ToString() const { return GetName(); }
 
-  inline bool HasBeenHandled() { return has_been_handled_; }
+  /// @brief Checks if the Event has been handled.
+  bool HasBeenHandled() { return has_been_handled_; }
 
-  inline bool IsInCategory(EventCategory category) {
+  /// @brief Checks if the Event belongs to a specific category.
+  bool IsInCategory(EventCategory category) {
       return GetCategoryFlags() & category; }
 
-  inline std::ostream& operator<<(std::ostream& os) {
-      return os << ToString(); }
+  /// @brief Support the use of << with Events.
+  std::ostream& operator<<(std::ostream& os) { return os << ToString(); }
 
  protected:
   bool has_been_handled_ = false;
-  inline void SetHandled(const bool success) { has_been_handled_ = success; }
+  void SetHandled(const bool success) { has_been_handled_ = success; }
 };
 
+/// @brief The primary way of allowing the application and layers in lambda
+/// the capability of handling events propagated throughout the application.
 class EventDispatcher {
   template<typename T>
   using EventFn = const std::function<bool(const T&)>;
@@ -84,10 +94,13 @@ class EventDispatcher {
  public:
   explicit EventDispatcher(memory::Shared<Event> event) : event_(event) {}
 
+  /// @brief Dispatch an event to be handled if it matches the class Event
   template<class Event>
   bool Dispatch(EventFn<Event> func) {
     if (event_->GetEventType() == Event::GetStaticType()) {
-      event_->SetHandled(func(dynamic_cast<const Event&>(*event_)));
+      const Event& casted_event = dynamic_cast<const Event&>(*event_);
+      bool success = func(casted_event);
+      event_->SetHandled(success);
       return true;
     }
     return false;
