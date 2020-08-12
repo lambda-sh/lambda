dea2d7fda6436c28f5da0abbdcc72275bef565f1
C3NZ <cenz@cenz.io>
2020-08-11 20:27:19 -0700

    [fix] the documentation for the imgui implementation.

--- lambda/src/core/imgui/ImGuiLayer.h
+++ lambda/src/core/imgui/ImGuiLayer.h
@@ -1,11 +1,9 @@
-/**
- * @file ImGuiLayer.h
- * @brief The ImGuiLayer implementation for dev tool creation.
- *
- * Any application that inherits from the game engine should not compile with
- * imgui. It is very performance heavy and will cause your application to
- * perform magnitudes slower.
- */
+/// @file ImGuiLayer.h
+/// @brief The ImGuiLayer implementation for dev tool creation.
+///
+/// Any application that inherits from the game engine should not compile with
+/// imgui. It is very performance heavy and will cause your application to
+/// perform magnitudes slower.
 #ifndef LAMBDA_SRC_CORE_IMGUI_IMGUILAYER_H_
 #define LAMBDA_SRC_CORE_IMGUI_IMGUILAYER_H_
 
@@ -20,16 +18,23 @@ namespace lambda {
 namespace core {
 namespace imgui {
 
+/// @brief The base ImGui layer used for rendering all other ImGui
+/// components.
 class ImGuiLayer : public layers::Layer {
  public:
   ImGuiLayer();
   ~ImGuiLayer();
 
+  /// @brief What to do when attached to lambda.
   void OnAttach() override;
+  /// @brief What to do when detached from lambda.
   void OnDetach() override;
+  /// @brief What to do when ImGui requests to render.
   void OnImGuiRender() override;
 
+  /// @brief Begin an ImGui rendering context.
   void Begin();
+  /// @brief End an ImGui rendering context.
   void End();
 
  private:
@@ -42,47 +47,3 @@ class ImGuiLayer : public layers::Layer {
 }  // namespace lambda
 
 #endif  // LAMBDA_SRC_CORE_IMGUI_IMGUILAYER_H_
-
-/**
- * @class lambda::imgui::ImGuiLayer
- * @brief An abstract Imgui layer implementation
- */
-
-/**
- * @fn lambda::imgui::ImGuiLayer::OnAttach
- * @brief Handles an ImGuiLayers attachment to the engine.
- *
- * This is currently setup with a default implementation but will most likely
- * be delegated to users that would like to implement their own imgui layers.
- */
-
-/**
- * @fn lambda::imgui::ImGuiLayer::OnDetach
- * @brief Handles an ImGuiLayers detachment to the engine.
- *
- * This is currently setup with a default implementation but will most likely
- * be delegated to users that would like to implement their own imgui layers.
- */
-
-/**
- * @fn lambda::imgui::ImGuiLayer::OnImGuiRender
- * @brief Handles an ImGuiRender Call by the engine.
- *
- * This will only be called when the engine is compiled with imgui attached.
- */
-
-/**
- * @fn lambda::imgui::ImGuiLayer::Begin
- * @brief Instantiates the context for the Imgui layer.
- *
- * Must be closed out with End() in order to prevent the imgui context from
- * leaking and interfering with other graphics trying to be rendered.
- */
-
-/**
- * @fn lambda::imgui::ImGuiLayer::End
- * @brief Closes and cleans up the context for the Imgui layer.
- *
- * Must be opened with Begin() in order to prevent the imgui context from
- * leaking and interfering with other graphics trying to be rendered.
- */
