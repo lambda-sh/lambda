b394b8ea59144cc3a822f9d540ead5d2ed84003b
C3NZ <cenz@cenz.io>
2020-08-17 12:59:04 -0700

    [add] documentation changes and update the doxyfile so that the input folder for doxygen is using the lambda directory as opposed to the game-engine one.

--- lambda/src/core/io/AsyncTask.h
+++ lambda/src/core/io/AsyncTask.h
@@ -1,3 +1,6 @@
+/// @file AsyncTask.h
+/// @brief A convenient wrapper for callback functions being dispatched into the
+/// event loop
 // TODO(C3NZ): Add documentation for this file.
 #ifndef LAMBDA_SRC_CORE_IO_ASYNCTASK_H_
 #define LAMBDA_SRC_CORE_IO_ASYNCTASK_H_
