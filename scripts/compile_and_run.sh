2b1344ee4fa9eeb128ad270b192981acc2a73a8e
C3NZ <cenz@cenz.io>
2020-08-20 13:25:44 -0700

    [add] the arg parser and implement it within the compile and run script. This will allow for the rapid development of bash scripts for the engine that can take in flags, log output, and eventually do some other useful developer related things.

--- scripts/compile_and_run.sh
+++ scripts/compile_and_run.sh
@@ -5,17 +5,23 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 pushd $ROOT_DIR
 
-# This sources the lambda utility script.
+# -------------------- SETUP ARGUMENTS AND LAMBDA SHELL ------------------------
+
 source scripts/lambda.sh
 
-# -------------------------
+LAMBDA_PARSE_ARG t tool sandbox "The tool to compile and run."
+LAMBDA_PARSE_ARG b build Release "The type of build to produce."
+
+LAMBDA_COMPILE_ARGS $@
+
+# -------------------- COMPILE THE ENGINE AND ALL TOOLS ------------------------
 
 export CC=gcc CXX=g++
 
-if [ "$1" = "Release" ] || [ "$1" = "Debug" ]; then
-    LAMBDA_INFO "Compiling a $1 build for the engine."
+if [ $LAMBDA_build = "Release" ] || [ $LAMBDA_build = "Debug" ]; then
+    LAMBDA_INFO "Compiling a $LAMBDA_build build for the engine."
     cmake \
-        -DCMAKE_BUILD_TYPE="$1" \
+        -DCMAKE_BUILD_TYPE="$LAMBDA_build" \
         -DDISTRIBUTION_BUILD=False \
         -DENGINE_DEVELOPMENT_MODE=True .
 elif [ "$1" = "Dist" ]; then
@@ -30,9 +36,9 @@ LAMBDA_INFO "Compiling the engine with make -j 8."
 make -j 8
 
 # Go to the output binary and run it.
-pushd "$1"/bin
-./app
+pushd "$LAMBDA_build"/bin
+./"$LAMBDA_tool"
 popd  # "$1/bin"
 popd  # ROOT_DIR
 
-LAMBDA_INFO "The tool and engine have been shutdown."
+LAMBDA_INFO "$LAMBDA_tool and engine have been shutdown."
