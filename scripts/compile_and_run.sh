f94281ba0d7ea90b236b2c4cd0810dd37464b721
C3NZ <cenz@cenz.io>
2020-08-20 16:44:50 -0700

    [add] more flags to make the script more customizable.

--- scripts/compile_and_run.sh
+++ scripts/compile_and_run.sh
@@ -1,7 +1,7 @@
 # compile_and_run.sh
 #
 # Compile and run one of the tools that the engine comes provided with in the
-# tools folder.
+# tools folder. This has been built and tested with bash 5.0.18
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 pushd $ROOT_DIR
 
@@ -11,12 +11,15 @@ source scripts/lambda.sh
 
 LAMBDA_PARSE_ARG t tool sandbox "The tool to compile and run."
 LAMBDA_PARSE_ARG b build Release "The type of build to produce."
+LAMBDA_PARSE_ARG c cores 8 "The amount of cores to use for compiling."
+LAMBDA_PARSE_ARG "" c-compiler gcc "The compiler to use for C code."
+LAMBDA_PARSE_ARG "" cpp-compiler g++ "The compiler to use for C++ code."
 
 LAMBDA_COMPILE_ARGS $@
 
 # -------------------- COMPILE THE ENGINE AND ALL TOOLS ------------------------
 
-export CC=gcc CXX=g++
+export CC=$LAMBDA_cpp_compiler CXX=$LAMBDA_cpp_compiler
 
 if [ $LAMBDA_build = "Release" ] || [ $LAMBDA_build = "Debug" ]; then
     LAMBDA_INFO "Compiling a $LAMBDA_build build for the engine."
@@ -33,10 +36,10 @@ else
 fi
 
 LAMBDA_INFO "Compiling the engine with make -j 8."
-make -j 8
+make -j $LAMBDA_cores
 
 # Go to the output binary and run it.
-pushd "$LAMBDA_build"/bin
+pushd "$LAMBDA_build/bin"
 ./"$LAMBDA_tool"
 popd  # "$1/bin"
 popd  # ROOT_DIR
