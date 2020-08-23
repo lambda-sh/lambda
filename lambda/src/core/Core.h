50780562bceb804a30911d19107261d700284c2a
C3NZ <cenz@cenz.io>
2020-08-23 13:01:40 -0700

    [add] the rest of the documentation that I've changed and updated. Also update the Doxyfile to expand MACROS to it's best ability.

--- lambda/src/core/Core.h
+++ lambda/src/core/Core.h
@@ -1,18 +1,34 @@
-/**
- * @file Core.h
- * @brief Core macros to be used all throughout the engine for development and
- * production purposes.
- *
- * This header includes core macros that are used all throughout the engine for
- * many different internal usage. You most likely won't need to use anything
- * from this file specifically and instead might find `core/Log.h` and
- * `core/Assert.h` more useful as they extend core and provide logging and
- * assertions (If enabled).
- */
+/// @file Core.h
+/// @brief Core macros to be used all throughout the engine for development and
+/// production purposes.
+///
+/// This header includes core macros that are used all throughout the engine for
+/// many different internal usage. You most likely won't need to use anything
+/// from this file specifically and instead might find `core/Log.h` and
+/// `core/Assert.h` more useful as they extend core and provide logging and
+/// assertions (If enabled).
 #ifndef LAMBDA_SRC_CORE_CORE_H_
 #define LAMBDA_SRC_CORE_CORE_H_
 
 #undef linux
+
+/// @def LAMBDA_API
+/// @brief Handles exporting the engine if it's being built as a dll.
+/// Potentially deprecated and shouldn't be used outside of the engine.
+
+/// @def LAMBDA_ENABLE_ASSERTS
+/// @brief Allows the usage of `core/Assert.h` Assertions to execute in the
+/// engine.
+
+/// @def LAMBDA_DEBUG_BREAK()
+/// @brief Allows the execution of breakpoints throughout the engine when
+/// debuggin.
+
+/// @def BIT(x)
+/// @param x The number to represent in bits.
+/// @brief Creates the bit representation of an integer.
+///
+/// Mostly for internal usage (Check `engine/src/core/events/Event.h`)
 
 #ifdef LAMBDA_PLATFORM_WINDOWS
   #ifdef LAMBDA_BUILD_DLL
@@ -49,29 +65,3 @@
 #define BIT(x) (1 << x)
 
 #endif  // LAMBDA_SRC_CORE_CORE_H_
-
-/**
- * @def LAMBDA_API
- * @brief Handles exporting the engine if it's being built as a dll.
- * Potentially deprecated and shouldn't be used outside of the engine.
- */
-
-/**
- * @def LAMBDA_ENABLE_ASSERTS
- * @brief Allows the usage of `core/Assert.h` Assertions to execute in the
- * engine.
- */
-
-/**
- * @def LAMBDA_DEBUG_BREAK()
- * @brief Allows the execution of breakpoints throughout the engine when
- * debuggin.
- */
-
-/**
- * @def BIT(x)
- * @param x The number to represent in bits.
- * @brief Creates the bit representation of an integer.
- *
- * Mostly for internal usage (Check `engine/src/core/events/Event.h`)
- */
