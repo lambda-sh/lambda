efe4078839373d06f1ddc09e95cfde4fe1c927c2
C3NZ <cenz@cenz.io>
2020-09-05 17:04:58 -0700

    [update] the logging system to include the file path, give better assertion errors, and overall be more useful than previously.

--- lambda/src/Lambda/core/util/Log.cpp
+++ lambda/src/Lambda/core/util/Log.cpp
@@ -11,7 +11,7 @@ std::shared_ptr<spdlog::logger> Log::kCoreLogger;
 std::shared_ptr<spdlog::logger> Log::kClientLogger;
 
 void Log::Init() {
-  spdlog::set_pattern("%^[%T] %n: %v%$");
+  spdlog::set_pattern("%^[%T]-[%n]-[%s]: %v%$");
   kCoreLogger = spdlog::stdout_color_mt("Engine");
   kCoreLogger->set_level(spdlog::level::trace);
 
