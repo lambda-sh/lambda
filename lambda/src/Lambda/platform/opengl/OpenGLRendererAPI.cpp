e8c6f373cefbfe95224a01f8dc65ad2f17bc228c
C3NZ <cenz@cenz.io>
2020-09-08 12:55:16 -0700

    [update] the renderer to only use a single shader for rendering, as unbinding and binding shaders can get expensive.

--- lambda/src/Lambda/platform/opengl/OpenGLRendererAPI.cpp
+++ lambda/src/Lambda/platform/opengl/OpenGLRendererAPI.cpp
@@ -8,13 +8,17 @@ namespace opengl {
 void OpenGLRendererAPI::Init() {
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  // Enables depth on a per pixel basis.
+  glEnable(GL_DEPTH_TEST);
 }
 
 void OpenGLRendererAPI::SetClearColor(const glm::vec4& color) {
   glClearColor(color.r, color.g, color.b, color.a);
 }
 
-void OpenGLRendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) {
+void OpenGLRendererAPI::SetViewport(
+    uint32_t x, uint32_t y, uint32_t width, uint32_t height) {
   glViewport(x, y, width, height);
 }
 
