45ed265f30c84a45bdd0362f558378b76274eba1
cenz <cenz@cenz.io>
2020-12-23 18:33:22 -0800

    [update] the setup script to exit if it's encapsulated within a ci environment.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,12 +3,36 @@
 # Setup lambda for development. This currently doesn't do too much, but should
 # be run at least ONCE after cloning the repository.
 ROOT_DIR="$(git rev-parse --show-toplevel)"
-pushd $ROOT_DIR
+pushd "$ROOT_DIR" > /dev/null
 
-# Install pre-commit configs for working on the project.
-pre-commit install
+# ------------------------------ UPDATE SUBMODULES -----------------------------
 
 # Load git submodules.
-git submodule update --init
+git submodule update --init --recursive
+
+# ------------------------------- LAMBDA-SH SETUP ------------------------------
+
+source lambda-sh/lambda.sh
+
+LAMBDA_INFO "Successfully installed submodules and setup lambda.sh"
+
+LAMBDA_PARSE_ARG within-ci false \
+    "Used when setup is being done within a CI system."
+LAMBDA_COMPILE_ARGS $@
+
+if [ "$LAMBDA_within_ci" = true ]; then
+    exit
+fi
+
+# ------------------------------ PRE-COMMIT SETUP ------------------------------
+
+if command -v pre-commit > /dev/null; then
+    pre-commit install > /dev/null
+    LAMBDA_INFO "Installed pre-commit hooks."
+else
+    LAMBDA_FATAL "pre-commit isn't installed. Aborting setup."
+fi
+
+LAMBDA_INFO "Setup successfully completed."
 
 popd  # ROOT_DIR
