d3ec0137ef86c28dff1dffddbc719b1b9ce1c7d1
C3NZ <cenz@cenz.io>
2021-02-15 15:30:08 -0800

    [add] a basic tool to start testing out the math portion of the library.

---
+++ tools/math/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Lambda/Lambda.h>
+#include <Lambda/core/Entrypoint.h>
+#include <Lambda/math/Vector.h>
+
+using lambda::core::memory::Unique;
+using lambda::core::memory::CreateUnique;
+
+int TestMath() {
+  lambda::math::Vector<float> v({ 3.0, 3.0 });
+
+  auto values = v.GetRawElements();
+
+  for (auto value : values) {
+    LAMBDA_CORE_INFO("Value in Vector: {0}", value);
+  }
+}
+
+Unique<Application> lambda::core::CreateApplication() {
+  TestMath();
+  return memory::CreateUnique<Application>();
+}
