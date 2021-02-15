e781c5728d1dbd5d5a9a3ad66b3ecdf918c83522
C3NZ <cenz@cenz.io>
2021-02-15 15:28:07 -0800

    [add] a basic vector api for the initial math library testing.

---
+++ lambda/src/Lambda/math/Vector.h
@@ -0,0 +1,29 @@
+#ifndef LAMBDA_SRC_LAMBDA_MATH_VECTOR_H_
+#define LAMBDA_SRC_LAMBDA_MATH_VECTOR_H_
+
+#include <stdint.h>
+#include <vector>
+
+#include <lambda/core/memory/Pointers.h>
+
+namespace lambda {
+namespace math {
+
+template<class T>
+class Vector {
+ public:
+  Vector(size_t size, std::vector<T> elements)
+    : size_(size), elements_(elements) {}
+  explicit Vector(std::vector<T> elements) : elements_(elements) {}
+
+  const std::vector<T>& const GetRawElements() { return elements_; }
+
+ private:
+  size_t size_;
+  std::vector<T> elements_;
+};
+
+}  // namespace math
+}  // namespace lambda
+
+#endif  // LAMBDA_SRC_LAMBDA_MATH_VECTOR_H_
