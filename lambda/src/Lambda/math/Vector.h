60bf953b564e597f65c7c34f8f4fac659388d3bf
C3NZ <cenz@cenz.io>
2021-03-13 14:33:02 -0800

    [update] Vector3 to use std::array for allocations.

--- lambda/src/Lambda/math/Vector.h
+++ lambda/src/Lambda/math/Vector.h
@@ -12,7 +12,7 @@ namespace lambda::math {
 /// @brief Implementation for Vectors of varying length.
 /// @tparam Type is the type of the element being stored within Container.
 /// @tparam Container The container to use for storing elements within.
-template<class Type = Real, class Container = std::vector<Type>>
+template<class Type = Real, class Container = std::vector<Real>>
 class Vector {
  public:
   Vector(const size_t size, Container elements)
@@ -31,19 +31,17 @@ class Vector {
 };
 
 /// @brief Implementation for Vectors of length 3.
-class Vector3 : Vector<Real, std::array<Real, 3>> {
+class Vector3 : public Vector<Real, std::array<Real, 3>> {
  public:
-  Vector3() : Vector(3, {0, 0, 0}) {}
-  Vector3(const Real x, const Real y, const Real z)
-    : Vector(3, {x, y, z}) {}
+  Vector3() : Vector({0, 0, 0}) {}
 
   inline void SetX(const Real x) { elements_[0] = x; }
   [[nodiscard]] inline Real GetX() const { return elements_[0]; }
 
-  inline void SetY(const Real y) { elements_[0] = y; }
+  inline void SetY(const Real y) { elements_[1] = y; }
   [[nodiscard]] inline Real GetY() const { return elements_[1]; }
 
-  inline void SetZ(const Real z) { elements_[0] = z; }
+  inline void SetZ(const Real z) { elements_[2] = z; }
   [[nodiscard]] inline Real GetZ() const { return elements_[2]; }
 };
 
