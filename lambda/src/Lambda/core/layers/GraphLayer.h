87691c364635e3629b614b2958e023f47188b649
C3NZ <cenz@cenz.io>
2021-04-12 12:01:59 -0700

    [update] initial implementation for 2D graph rendering and remove the 3D graph layer for now.

--- lambda/src/Lambda/core/layers/GraphLayer.h
+++ lambda/src/Lambda/core/layers/GraphLayer.h
@@ -2,46 +2,67 @@
 #define LAMBDA_SRC_LAMBDA_CORE_LAYERS_GRAPHLAYER_H_
 
 #include <Lambda/concepts/Plot.h>
+#include <Lambda/core/OrthographicCameraController.h>
 #include <Lambda/core/layers/Layer.h>
 #include <Lambda/core/renderer/Buffer.h>
+#include <Lambda/core/renderer/RenderCommand.h>
+#include <Lambda/core/renderer/Renderer.h>
+#include <Lambda/core/renderer/Renderer2D.h>
+#include <Lambda/math/plot/Graph.h>
 
 namespace lambda::core::layers {
-
-template <concepts::Graph Plot>
 class GraphLayer2D : public Layer {
  public:
-  explicit GraphLayer2D(
-    Plot graph)
-        : Layer("Graph2D-Layer"), graph_(std::move(graph)) {}
+  explicit GraphLayer2D(math::plot::Graph2D<> graph)
+      : Layer("Graph2D-Layer"),
+      graph_(graph),
+      camera_controller_(1280.0f / 720.0f, true) {}
 
-  explicit GraphLayer2D(Plot&& graph)
-      : Layer("Graph2D-Layer"), graph_(std::move(graph)) {}
+  explicit GraphLayer2D(math::plot::Graph2D<>&& graph)
+      : Layer("Graph2D-Layer"),
+      graph_(std::move(graph)),
+      camera_controller_(1280.0f / 720.0f, true) {}
 
-  void OnAttach() override;
-  void OnDetach() override;
-  void OnUpdate(util::TimeStep time_step) override;
-  void OnEvent(memory::Shared<events::Event> event) override;
-  void OnImGuiRender() override;
+  void OnAttach() override {
+    for (auto& point : graph_.GetPoints()) {
+      points_.push_back(point.x);
+      points_.push_back(point.y);
+    }
+  }
+
+  void OnDetach() override {
+  }
+
+  void OnUpdate(util::TimeStep time_step) override {
+    camera_controller_.OnUpdate(time_step);
+
+    renderer::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+    renderer::RenderCommand::Clear();
+
+    renderer::Renderer2D::BeginScene(
+        camera_controller_.GetOrthographicCamera());
+
+    for (auto& point : graph_.GetPoints()) {
+      renderer::Renderer2D::DrawQuad(
+        {point.x, point.y}, {0.5f, 0.5f}, {1.0f, 0.6f, 0.2f, 1.0f});
+    }
+
+    renderer::Renderer2D::EndScene();
+  };
+
+  void OnEvent(memory::Shared<events::Event> event) override {
+    camera_controller_.OnEvent(event);
+  }
+
+  void OnImGuiRender() override {}
+
  private:
-  Plot graph_;
-};
-
-template<concepts::Graph Plot>
-class GraphLayer3D : public Layer {
- public:
-  explicit GraphLayer3D(Plot graph)
-          : Layer("Graph3D-Layer"), graph_(std::move(graph)) {}
-
-  explicit GraphLayer3D(Plot&& graph)
-          : Layer("Graph3D-Layer"), graph_(std::move(graph)) {}
-
-  void OnAttach() override;
-  void OnDetach() override;
-  void OnUpdate(util::TimeStep time_step) override;
-  void OnEvent(memory::Shared<events::Event> event) override;
-  void OnImGuiRender() override;
- private:
-  Plot graph_;
+  math::plot::Graph2D<> graph_;
+  memory::Shared<renderer::VertexArray> point_array_;
+  memory::Shared<renderer::Shader> point_shader_;
+  memory::Shared<renderer::VertexBuffer> point_buffer_;
+  OrthographicCameraController camera_controller_;
+  std::vector<float> points_;
 };
 
 }  // namespace lambda::core::layers
