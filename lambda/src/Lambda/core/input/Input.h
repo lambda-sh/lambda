e61e547f20146ed94eeba9ee3bcf13e4cf233cdb
C3NZ <cenz@cenz.io>
2021-04-14 20:26:38 -0700

    [refactor] the Input API.

--- lambda/src/Lambda/core/input/Input.h
+++ lambda/src/Lambda/core/input/Input.h
@@ -5,47 +5,50 @@
 
 #include <utility>
 
-#include "Lambda/core/Core.h"
+#include <Lambda/core/memory/Pointers.h>
 
-namespace lambda {
-namespace core {
-namespace input {
+namespace lambda::core::input {
 
 /// @brief The generic input system for getting input data from
 /// applications built on lambda.
 class Input {
  public:
   /// @brief Check if a lambda key code was pressed.
-  static bool IsKeyPressed(int key_code) {
-      return kInput_->IsKeyPressedImpl(key_code); }
+  [[nodiscard]] static bool IsKeyPressed(const int key_code) {
+    return kInput_->IsKeyPressedImpl(key_code);
+  }
 
   /// @brief Get the current Mouse X Coordinate.
-  static float GetMouseX() { return kInput_->GetMouseXImpl(); }
+  [[nodiscard]] static float GetMouseX() {
+    return kInput_->GetMouseXImpl();
+  }
   /// @brief Get the current Mouse Y Coordinate.
-  static float GetMouseY() { return kInput_->GetMouseYImpl(); }
+  [[nodiscard]] static float GetMouseY() {
+    return kInput_->GetMouseYImpl();
+  }
 
   /// @brief Get the current mouse position (X, Y) as a pair.
-  static std::pair<float, float> GetMousePosition() {
-      return kInput_->GetMousePositionImpl(); }
+  [[nodiscard]] static std::pair<float, float> GetMousePosition() {
+      return kInput_->GetMousePositionImpl();
+  }
 
   /// @brief Check to see if a mouse button is being pressed.
-  static bool IsMouseButtonPressed(int button) {
-      return kInput_->IsMouseButtonPressedImpl(button); }
+  [[nodiscard]] static bool IsMouseButtonPressed(const int button) {
+      return kInput_->IsMouseButtonPressedImpl(button);
+  }
 
  protected:
-  virtual bool IsKeyPressedImpl(int key_code) = 0;
+  [[nodiscard]] virtual bool IsKeyPressedImpl(int key_code) = 0;
 
-  virtual float GetMouseXImpl() = 0;
-  virtual float GetMouseYImpl() = 0;
-  virtual std::pair<float, float> GetMousePositionImpl() = 0;
-  virtual bool IsMouseButtonPressedImpl(int button) = 0;
+  [[nodiscard]] virtual float GetMouseXImpl() = 0;
+  [[nodiscard]] virtual float GetMouseYImpl() = 0;
+  [[nodiscard]] virtual std::pair<float, float> GetMousePositionImpl() = 0;
+  [[nodiscard]] virtual bool IsMouseButtonPressedImpl(int button) = 0;
 
  private:
-  static Input* kInput_;
+  static memory::Unique<Input> kInput_;
 };
 
-}  // namespace input
-}  // namespace core
-}  // namespace lambda
+}  // namespace lambda::core::input
 
 #endif  // LAMBDA_SRC_LAMBDA_CORE_INPUT_INPUT_H_
