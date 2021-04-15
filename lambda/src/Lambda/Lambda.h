9fde0ad2230e036f45c448658c3d6b3c1231b73e
C3NZ <cenz@cenz.io>
2021-04-14 21:14:06 -0700

    [add] math API to lambda global header and update include directives.

--- lambda/src/Lambda/Lambda.h
+++ lambda/src/Lambda/Lambda.h
@@ -7,42 +7,49 @@
 
 // ------------------------------------ CORE -----------------------------------
 
-#include "Lambda/core/Application.h"
-#include "Lambda/core/OrthographicCameraController.h"
-#include "Lambda/core/events/Event.h"
-#include "Lambda/core/imgui/ImGuiLayer.h"
-#include "Lambda/core/layers/Layer.h"
+#include <Lambda/core/Application.h>
+#include <Lambda/core/OrthographicCameraController.h>
+#include <Lambda/core/events/Event.h>
+#include <Lambda/core/imgui/ImGuiLayer.h>
+#include <Lambda/core/layers/Layer.h>
 
 // ------------------------------------ INPUT ----------------------------------
 
-#include "Lambda/core/input/Input.h"
-#include "Lambda/core/input/KeyCodes.h"
-#include "Lambda/core/input/MouseButtonCodes.h"
+#include <Lambda/core/input/Input.h>
+#include <Lambda/core/input/KeyCodes.h>
+#include <Lambda/core/input/MouseButtonCodes.h>
 
 // ------------------------------------ UTIL -----------------------------------
 
-#include "Lambda/core/util/Assert.h"
-#include "Lambda/core/util/Log.h"
-#include "Lambda/core/util/Reverse.h"
-#include "Lambda/core/util/Time.h"
+#include <Lambda/core/util/Assert.h>
+#include <Lambda/core/util/Log.h>
+#include <Lambda/core/util/Reverse.h>
+#include <Lambda/core/util/Time.h>
 
 // ------------------------------------- IO ------------------------------------
 
-#include "Lambda/core/io/EventLoop.h"
-#include "Lambda/core/io/AsyncTask.h"
+#include <Lambda/core/io/EventLoop.h>
+#include <Lambda/core/io/AsyncTask.h>
 
 // ---------------------------------- MEMORY -----------------------------------
 
-#include "Lambda/core/memory/Pointers.h"
+#include <Lambda/core/memory/Pointers.h>
 
 // --------------------------------- RENDERING ---------------------------------
 
-#include "Lambda/core/renderer/Buffer.h"
-#include "Lambda/core/renderer/OrthographicCamera.h"
-#include "Lambda/core/renderer/RenderCommand.h"
-#include "Lambda/core/renderer/Renderer.h"
-#include "Lambda/core/renderer/Shader.h"
-#include "Lambda/core/renderer/Texture.h"
-#include "Lambda/core/renderer/VertexArray.h"
+#include <Lambda/core/renderer/Buffer.h>
+#include <Lambda/core/renderer/OrthographicCamera.h>
+#include <Lambda/core/renderer/RenderCommand.h>
+#include <Lambda/core/renderer/Renderer.h>
+#include <Lambda/core/renderer/Shader.h>
+#include <Lambda/core/renderer/Texture.h>
+#include <Lambda/core/renderer/VertexArray.h>
+
+// ----------------------------------- MATH ------------------------------------
+
+#include <Lambda/math/Precision.h>
+#include <Lambda/math/Vector.h>
+#include <Lambda/math/plot/Graph.h>
+#include <Lambda/math/shapes/Point.h>
 
 #endif  // LAMBDA_SRC_LAMBDA_LAMBDA_H_
