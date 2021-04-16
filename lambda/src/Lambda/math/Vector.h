1d13e7b6e29976e06121dfd180d65444db051cb0
C3NZ <cenz@cenz.io>
2021-04-16 13:18:52 -0700

    [add] Vector2 implementation and change Vector

--- lambda/src/Lambda/math/Vector.h
+++ lambda/src/Lambda/math/Vector.h
@@ -14,40 +14,89 @@ namespace lambda::math {
 template<class Type = Real, class Container = std::vector<Real>>
 class Vector {
  public:
-  Vector(const size_t size, Container elements)
-    : size_(size), elements_(std::move(elements)) {}
-
+  Vector() : elements_(Container()) {}
   explicit Vector(Container elements)
-    : size_(elements.size()), elements_(std::move(elements)) {}
+    : elements_(std::move(elements)) {}
 
-  const Container& GetRawElements() { return elements_; }
+  const Container& GetRawElements() {
+    return elements_;
+  }
 
-  [[nodiscard]] size_t GetSize() const { return size_; }
+  [[nodiscard]] size_t GetSize() const {
+    return elements_.size();
+  }
+
+  void ApplyInPlace(std::function<Type(Type)> lambda) {
+    std::for_each(elements_.begin(), elements_.end(), lambda);
+  }
+
+  Container Apply(std::function<Type(Type)> lambda) {
+    Container new_elements(elements_.size());
+    std::transform(elements_.begin(), elements_.end(), &new_elements, lambda);
+    return new_elements;
+  }
 
  protected:
-  size_t size_;
   Container elements_;
+};
+
+class Vector2 : public Vector<Real, std::array<Real, 2>> {
+ public:
+  Vector2() : Vector({0, 0}) {}
+  Vector2(const Real x, const Real y) : Vector({x, y}) {}
+  explicit Vector2(const std::array<Real, 2> elements) : Vector(elements) {}
+
+  /// @brief Set the x component of the current vector.
+  void SetX(const Real x) {
+    elements_[0] = x;
+  }
+  [[nodiscard]] Real GetX() const {
+    return elements_[0];
+  }
+
+  /// @brief Set the y component of the current vector.
+  void SetY(const Real y) {
+    elements_[1] = y;
+  }
+
+  [[nodiscard]] Real GetY() const {
+    return elements_[1];
+  }
 };
 
 /// @brief Implementation for Vectors of length 3.
 class Vector3 : public Vector<Real, std::array<Real, 3>> {
  public:
   Vector3() : Vector({0, 0, 0}) {}
+  explicit Vector3(const std::array<Real, 3> elements) : Vector(elements) {}
+  Vector3(const Real x, const Real y) : Vector({x, y}) {}
+
 
   /// @brief Set the x component of the current vector.
-  /// @param x
-  inline void SetX(const Real x) { elements_[0] = x; }
-  [[nodiscard]] inline Real GetX() const { return elements_[0]; }
+  void SetX(const Real x) {
+    elements_[0] = x;
+  }
+  [[nodiscard]] Real GetX() const {
+    return elements_[0];
+  }
 
   /// @brief Set the y component of the current vector.
-  /// @param y
-  inline void SetY(const Real y) { elements_[1] = y; }
-  [[nodiscard]] inline Real GetY() const { return elements_[1]; }
+  void SetY(const Real y) {
+    elements_[1] = y;
+  }
+
+  /// @brief Get the y component of the current vector.
+  [[nodiscard]] Real GetY() const {
+    return elements_[1];
+  }
 
   /// @brief Set the z component of the current vector.
-  /// @param z
-  inline void SetZ(const Real z) { elements_[2] = z; }
-  [[nodiscard]] inline Real GetZ() const { return elements_[2]; }
+  void SetZ(const Real z) {
+    elements_[2] = z;
+  }
+  [[nodiscard]] Real GetZ() const {
+    return elements_[2];
+  }
 };
 
 }  // namespace lambda::math
