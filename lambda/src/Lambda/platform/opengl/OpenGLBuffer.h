c1e210a751d334fcd3b8159444847966ce3a4316
C3NZ <cenz@cenz.io>
2021-05-26 14:26:12 -0700

    [refactor] opengl headers.

--- lambda/src/Lambda/platform/opengl/OpenGLBuffer.h
+++ lambda/src/Lambda/platform/opengl/OpenGLBuffer.h
@@ -3,14 +3,11 @@
 #ifndef LAMBDA_SRC_LAMBDA_PLATFORM_OPENGL_OPENGLBUFFER_H_
 #define LAMBDA_SRC_LAMBDA_PLATFORM_OPENGL_OPENGLBUFFER_H_
 
-#include "Lambda/core/renderer/Buffer.h"
+#include <Lambda/core/renderer/Buffer.h>
 
-namespace lambda {
-namespace platform {
-namespace opengl {
+namespace lambda::platform::opengl {
 
 // ----------------------------- VERTEX BUFFER IMPL ----------------------------
-
 
 /// @brief The OpenGL VertexBuffer implementation based off the generic
 /// VertexBuffer base class provided by the engines renderer.
@@ -31,17 +28,18 @@ class OpenGLVertexBuffer : public core::renderer::VertexBuffer {
 
 
   /// @brief Get the BufferLayout associated with the current VertexBuffer.
-  const core::renderer::BufferLayout& GetLayout() const
-    override { return layout_; };
+  const core::renderer::BufferLayout& GetLayout() const override { 
+    return layout_; 
+  };
 
   /// @brief Set the BufferLayout associated with the current VertexBuffer.
-  void SetLayout(const core::renderer::BufferLayout& layout) override
-    { layout_ = layout; };
+  void SetLayout(const core::renderer::BufferLayout& layout) override { 
+    layout_ = layout; 
+  };
 
  private:
   uint32_t renderer_ID_;
   core::renderer::BufferLayout layout_;
-
 };
 
 // ----------------------------- INDEX BUFFER IMPL -----------------------------
@@ -61,9 +59,6 @@ class OpenGLIndexBuffer : public core::renderer::IndexBuffer {
   uint32_t renderer_ID_;
 };
 
-
-}  // namespace opengl
-}  // namespace platform
-}  // namespace lambda
+}  // namespace lambda::platform::opengl
 
 #endif  // LAMBDA_SRC_LAMBDA_PLATFORM_OPENGL_OPENGLBUFFER_H_
