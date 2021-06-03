d8101669f0764b6ea8c49f5ab638b2592c5aeb53
C3NZ <cenz@cenz.io>
2021-06-02 19:59:40 -0700

    [add] more vector3 operators

--- lambda/src/Lambda/math/Vector3.h
+++ lambda/src/Lambda/math/Vector3.h
@@ -10,10 +10,14 @@ namespace lambda::math {
 /// @brief Implementation for Vectors of length 3.
 class Vector3 : public Vector<Real, std::array<Real, 3>> {
  public:
-  Vector3() : Vector({0, 0, 0}) {}
-  explicit Vector3(const std::array<Real, 3> elements) : Vector(elements) {}
+  Vector3() noexcept : Vector({0, 0, 0}) {}
+  explicit Vector3(const std::array<Real, 3> elements)
+      noexcept : Vector(elements) {}
+
   Vector3(const Real x, const Real y, const Real z) noexcept
       : Vector({x, y, z}) {}
+
+
 
   /// @brief Set the x component of the current vector.
   void SetX(const Real x) {
@@ -78,6 +82,39 @@ class Vector3 : public Vector<Real, std::array<Real, 3>> {
         GetX() - other_vector.GetX(),
         GetY() - other_vector.GetY(),
         GetZ() - other_vector.GetZ());
+  }
+
+  void operator*=(const Vector3& other_vector) {
+    SetX(GetX() * other_vector.GetX());
+    SetY(GetY() * other_vector.GetY());
+    SetZ(GetX() * other_vector.GetZ());
+  }
+
+  Vector3 operator*(const Vector3& other_vector) const {
+    return Vector3(
+      GetX() * other_vector.GetX(),
+      GetY() * other_vector.GetY(),
+      GetZ() * other_vector.GetZ());
+  }
+
+  void operator/=(const Vector3& other_vector) {
+    SetX(GetX() / other_vector.GetX());
+    SetY(GetY() / other_vector.GetY());
+    SetZ(GetZ() / other_vector.GetZ());
+  }
+
+  Vector3 operator/(const Vector3& other_vector) {
+    return Vector3(
+      GetX() / other_vector.GetX(),
+      GetY() / other_vector.GetY(),
+      GetZ() / other_vector.GetZ());
+  }
+
+  friend Vector3 operator*(const Vector3& vector, Real scalar) const {
+    return Vector3(
+      vector.GetX() * scalar,
+      vector.GetY() * scalar,
+      vector.GetZ() * scalar);
   }
 };
 
