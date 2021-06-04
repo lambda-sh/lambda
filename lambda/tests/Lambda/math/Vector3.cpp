f2591c00ca56ceffa1671908ecf911c4e8594cdd
C3NZ <cenz@cenz.io>
2021-06-04 16:12:07 -0700

    [test] for default initialization of vec3s.

--- lambda/tests/Lambda/math/Vector3.cpp
+++ lambda/tests/Lambda/math/Vector3.cpp
@@ -2,6 +2,9 @@
 
 #include <Lambda/math/Vector3.h>
 
-TEST(Vector3, DefaultInitialization) {
+TEST(Vector3, DefaultInitializationOfZeros) {
   lambda::math::Vector3 vec;
+  ASSERT_EQ(vec.GetX(), 0);
+  ASSERT_EQ(vec.GetY(), 0);
+  ASSERT_EQ(vec.GetZ(), 0);
 }
