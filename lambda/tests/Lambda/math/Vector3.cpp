4cc7fe2a267c3333c6627592eecdaa2bce1bef00
C3NZ <cenz@cenz.io>
2021-06-04 16:22:15 -0700

    [test] initialization from std::array

--- lambda/tests/Lambda/math/Vector3.cpp
+++ lambda/tests/Lambda/math/Vector3.cpp
@@ -1,9 +1,19 @@
+#include <array>
+
 #include <gtest/gtest.h>
 
+#include <Lambda/math/Precision.h>
 #include <Lambda/math/Vector3.h>
 
 TEST(Vector3, DefaultInitializationOfZeros) {
   lambda::math::Vector3 vec;
+  ASSERT_EQ(vec.GetX(), 0);
+  ASSERT_EQ(vec.GetY(), 0);
+  ASSERT_EQ(vec.GetZ(), 0);
+}
+
+TEST(Vector3, InitializationFromArray) {
+  lambda::math::Vector3 vec(std::array<lambda::math::Real, 3>({0, 0, 0}));
   ASSERT_EQ(vec.GetX(), 0);
   ASSERT_EQ(vec.GetY(), 0);
   ASSERT_EQ(vec.GetZ(), 0);
