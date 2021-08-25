b1bcaccee28d6fb02943e2003f5a5b4aa3d29b66
C3NZ <cenz@cenz.io>
2021-08-25 15:16:19 -0700

    [add] Rust project scaffolding for rebuilding lambda.

---
+++ lambda/src/lambda.rs
@@ -0,0 +1,3 @@
+pub fn create_application() {
+    println!("First call into the lambda library from another package.")
+}
---
+++ tools/lambda_rs_demo/src/main.rs
@@ -0,0 +1,5 @@
+use lambda;
+
+fn main() {
+    lambda::create_application();
+}
