ed8448451f4baf040dcf1dedebbbaa71e4f737d4
C3NZ <cenz@cenz.io>
2021-08-29 11:07:46 -0700

    [update] import structure.

--- tools/lambda_rs_demo/src/main.rs
+++ tools/lambda_rs_demo/src/main.rs
@@ -1,12 +1,11 @@
-use lambda::core::application as app;
+use lambda::core as core;
 
 fn main() {
-
-    let configuration = app::ApplicationConfiguration{
+    let configuration = core::ApplicationConfiguration{
         name: String::from("Demo application"),
         start_on_create: false,
     };
 
-    let demo = app::create_application(configuration);
+    let demo = core::create_application(configuration);
     println!("{} is currently running.", demo.get_name());
 }
