9a1f23a5750e883dd8a5a4c0dae43cec8197ce8e
C3NZ <cenz@cenz.io>
2021-08-28 18:53:52 -0700

    [update] demo tool to utilize the current application structure.

--- tools/lambda_rs_demo/src/main.rs
+++ tools/lambda_rs_demo/src/main.rs
@@ -1,5 +1,13 @@
-use lambda;
+use lambda::core::ApplicationConfiguration;
+use lambda::core::create_application;
 
 fn main() {
-    lambda::create_application();
+
+    let configuration = ApplicationConfiguration{
+        name: String::from("Demo application"),
+        start_on_create: false,
+    };
+
+    let app = create_application(configuration);
+    println!("{} is currently running.", app.get_name());
 }
