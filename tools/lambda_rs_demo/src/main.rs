62028be8c876c30484c03c53e9c7a68965866d64
C3NZ <cenz@cenz.io>
2021-08-31 15:39:42 -0700

    [update] tool usage for lambda.

--- tools/lambda_rs_demo/src/main.rs
+++ tools/lambda_rs_demo/src/main.rs
@@ -1,12 +1,43 @@
+use lambda::{
+    core::create_default_application,
+    core::Application,
+    core::Runnable,
+    core::RunnableApplication,
+    core::start_application,
+};
+
+pub struct DemoApp;
+
+impl Application for DemoApp {
+    fn get_name(&self) -> std::string::String { return String::from("Demo app") }
+    fn get_window(&self) -> &lambda::core::LambdaWindow { todo!() }
+    fn is_running(&self) -> bool { todo!() }
+}
+
+impl Runnable for DemoApp {
+    fn setup(&self){
+        println!("Demo application runner setup!")
+    }
+    fn run(&self){
+        println!("Demo applicaiton runner loop!")
+    }
+    fn on_update(&self){}
+    fn on_render(&self){}
+    fn on_event(&self){}
+}
+
+impl DemoApp {
+    pub fn new() -> Self {
+        return DemoApp{};
+    }
+}
+
 fn main() {
-    use lambda::core::AppConfig;
-    use lambda::core::create_application;
+    let lambda = create_default_application();
+    println!("{} was just created", lambda.get_name());
+    start_application(lambda);
 
-    let configuration = AppConfig::from_default()
-        .with_name(String::from("Demo app"))
-        .with_window_size([720, 480])
-        .start_on_create(false);
-
-    let demo = create_application(configuration);
-    println!("{} is currently running.", demo.get_name());
+    let demo = DemoApp::new();
+    println!("{} was just created.", demo.get_name());
+    start_application(demo);
 }
