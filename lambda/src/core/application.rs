520a9bf577e5a13828c80a28616876f4d1f03790
C3NZ <cenz@cenz.io>
2021-09-05 12:51:08 -0700

    [add] Layering system to provide access into the LambdaRunnable event loop.

--- lambda/src/core/application.rs
+++ lambda/src/core/application.rs
@@ -1,53 +1,118 @@
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::ControlFlow;
+use std::time::{Duration, Instant};
 
-use crate::core::event_loop::{
-    LambdaEventLoop,
+use winit::{
+    event::Event,
+    event::WindowEvent,
+    event_loop::ControlFlow
 };
 
-use crate::core::window::{
-    Window,
-    LambdaWindow,
+use crate::{
+    core::event_loop::LambdaEvent,
+    core::event_loop::LambdaEventLoop,
+    core::window::Window,
+    core::window::LambdaWindow
 };
 
-use crate::core::event_loop::{LambdaEvent};
 
+pub trait Layer {
+    fn attach(&self);
+    fn detach(&self);
+    fn on_event(&self, event: &LambdaEvent);
+    fn on_update(&self, last_frame: &Duration);
+}
+
+pub struct DefaultLayer {}
+
+impl Layer for DefaultLayer {
+    fn attach(&self) {
+        println!("Attached to the lambda application");
+    }
+
+    fn detach(&self) {
+        todo!()
+    }
+
+    fn on_event(&self, event: &LambdaEvent) {
+    }
+
+    fn on_update(&self, last_frame: &Duration) {
+        println!("{} since last tick", last_frame.as_millis());
+    }
+}
+
+impl Default for DefaultLayer {
+    fn default() -> Self {
+        return DefaultLayer{};
+    }
+}
+
+pub struct LayerStack {
+    layers: Vec<Box<dyn Layer + 'static>>
+}
+
+impl LayerStack {
+    pub fn new() -> Self {
+        return LayerStack{
+            layers: Vec::new()
+        };
+    }
+
+    pub fn push_layer<T>(&mut self) 
+        where T: Default + Layer + 'static {
+            let layer = Box::new(T::default());
+            self.layers.push(layer);
+    }
+
+    pub fn pop_layer(&mut self) -> Option<Box<dyn Layer + 'static>> {
+        let layer = self.layers.pop();
+        return layer; 
+    }
+
+    pub fn on_event(&self, event: &LambdaEvent) {
+        for layer in &self.layers {
+            layer.on_event(&event);
+        }
+    }
+
+    pub fn on_update(&self, last_frame: &Duration) {
+        for layer in &self.layers {
+            layer.on_update(last_frame);
+        }
+    }
+}
 
 pub trait Runnable {
     fn setup(&self);
     fn run(self);
-    fn on_update(&self);
-    fn on_event(&self);
-}
-
-pub trait Layer {
-    fn on_attach(&self) {}
-    fn on_detach(&self) {}
-    fn on_update(&self) {}
-    fn on_event(&self, event: LambdaEvent) {}
 }
 
 pub struct LambdaRunnable {
     name: String,
     window: LambdaWindow,
     event_loop: LambdaEventLoop,
+    layer_stack: LayerStack,
 }
 
 impl LambdaRunnable {
-    pub fn new() -> Self {
+    pub fn with_layer_attached<T: Default + Layer + 'static>(mut self) -> Self { 
+        self.layer_stack.push_layer::<T>(); 
+        return self;
+    }
+}
+
+impl Default for LambdaRunnable {
+    fn default() -> Self {
         let name = String::from("LambdaRunnable");
         let event_loop = LambdaEventLoop::new();
         let window = LambdaWindow::new().with_event_loop(&event_loop);
+        let layer_stack = LayerStack::new();
 
         return LambdaRunnable{
             name,
             window,
             event_loop,
+            layer_stack
         }
-    }
-
-    pub fn get_name(&self) -> String {
-        return self.name.clone();
     }
 }
 
@@ -64,9 +129,16 @@ impl Runnable for LambdaRunnable {
         let app = self;
         let event_loop = app.event_loop;
         let window = app.window;
+        let layer_stack = app.layer_stack;
+
+        let mut last_frame= Instant::now();
+        let mut current_frame = Instant::now();
 
         event_loop.run_forever(
-                move |event, windows, control_flow| {
+                move |
+                        event, 
+                        _, 
+                        control_flow| {
                 match event {
                     Event::WindowEvent { event, .. } => {
                         match event {
@@ -94,6 +166,9 @@ impl Runnable for LambdaRunnable {
                         }
                     },
                     Event::MainEventsCleared => { 
+                        last_frame = current_frame;
+                        current_frame = Instant::now();
+                        layer_stack.on_update(&current_frame.duration_since(last_frame));
                     },
                     Event::RedrawRequested(_) => {
                         window.redraw();
@@ -118,19 +193,20 @@ impl Runnable for LambdaRunnable {
 
             });
     }
-
-    fn on_update(&self) {
-
-    }
-    fn on_event(&self) {}
 }
 
 
-pub fn create_lambda_application() -> LambdaRunnable {
-    return LambdaRunnable::new();
+pub fn create_lambda_runnable() -> LambdaRunnable {
+    return LambdaRunnable::default();
 }
 
-pub fn start_application<T: Runnable>(app: T) {
+pub fn build_and_start_runnable<T: Default + Runnable>() {
+    let app = T::default();
+
+    start_runnable(app);
+}
+
+pub fn start_runnable<T: Runnable>(app: T) {
     app.setup();
     app.run();
 }
--- tools/lambda_rs_demo/src/main.rs
+++ tools/lambda_rs_demo/src/main.rs
@@ -1,9 +1,38 @@
-use lambda::{
-    core::application::create_lambda_application,
-    core::application::start_application,
-};
+use lambda::core::{application::LambdaRunnable, application::{DefaultLayer, build_and_start_runnable, create_lambda_runnable, start_runnable}};
+use lambda::core::application::Layer;
+
+pub struct FirstLayer {
+    name: String,
+}
+
+impl Layer for FirstLayer {
+    fn attach(&self) {
+        println!("Attached the first layer to lambda");
+    }
+
+    fn detach(&self) {
+        todo!()
+    }
+
+    fn on_event(&self, event: &lambda::core::event_loop::LambdaEvent) {
+        todo!()
+    }
+
+    fn on_update(&self, last_frame: &std::time::Duration) {
+        println!("[layer 1] {} ns since last update", last_frame.as_nanos());
+    }
+}
+
+impl Default for FirstLayer {
+    fn default() -> Self {
+        return FirstLayer{name: String::from("Hello, lambda!")};
+    }
+}
+
 fn main() {
-    let lambda = create_lambda_application();
-    println!("{} was just created", lambda.get_name());
-    start_application(lambda);
+    let app = create_lambda_runnable()
+        .with_layer_attached::<FirstLayer>()
+        .with_layer_attached::<DefaultLayer>();
+
+    start_runnable(app);
 }
