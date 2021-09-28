4ccc9d6d5da4fa609178886adbad7fdc31637b1c
vmarcella <cenz@cenz.io>
2021-09-28 16:20:33 -0700

    [update] our gfx-rs platform implementation to include GPU & Instance implementations for streamlining the renderer initialization process.

---
+++ lambda/src/platform/gfx/gpu.rs
@@ -0,0 +1,170 @@
+use gfx_hal::{
+  adapter::{
+    Adapter,
+    Gpu,
+  },
+  command,
+  device::Device,
+  image::{
+    Access,
+    Layout,
+  },
+  memory::Dependencies,
+  pass::{
+    Attachment,
+    AttachmentLoadOp,
+    AttachmentOps,
+    AttachmentStoreOp,
+    SubpassDependency,
+    SubpassDesc,
+  },
+  pool::CommandPoolCreateFlags,
+  prelude::{
+    PhysicalDevice,
+    QueueFamily,
+  },
+  pso::PipelineStage,
+  queue::family,
+  window::Surface,
+};
+
+///
+/// Commands oriented around creating resources on & for the GPU.
+///
+
+pub struct GfxGpu<B: gfx_hal::Backend> {
+  adapter: Adapter<B>,
+  gpu: Gpu<B>,
+  command_pool: Option<B::CommandPool>,
+}
+
+#[derive(Clone, Copy)]
+pub enum RenderQueueType {
+  Compute,
+  Graphical,
+  GraphicalCompute,
+  Transfer,
+}
+
+/// Returns a callback for finding an internal adapter to use for GFX.
+fn is_queue_family_supported<B: gfx_hal::Backend>(
+  queue_family: &B::QueueFamily,
+  queue_type: RenderQueueType,
+  surface: Option<&B::Surface>,
+) -> bool {
+  match queue_type {
+    RenderQueueType::Compute => queue_family.queue_type().supports_compute(),
+    RenderQueueType::Graphical => match surface {
+      Some(surface) => {
+        surface.supports_queue_family(queue_family)
+          && queue_family.queue_type().supports_graphics()
+      }
+      None => false,
+    },
+    // TODO(vmarcella): These arms should be filled out to support the other kinds of queue types.
+    RenderQueueType::GraphicalCompute => todo!(),
+    RenderQueueType::Transfer => todo!(),
+  }
+}
+
+impl<B: gfx_hal::Backend> GfxGpu<B> {
+  /// Instantiates a new GPU given an adapter that is implemented by the GPUs
+  /// current rendering backend B. A new GPU does not come with a command pool unless specified.
+  pub fn new(
+    adapter: Adapter<B>,
+    queue_type: RenderQueueType,
+    surface: Option<&B::Surface>,
+  ) -> Self {
+    let queue_family = adapter
+      .queue_families
+      .iter()
+      .find(|family| {
+        return is_queue_family_supported::<B>(family, queue_type, surface);
+      })
+      .expect("No compatible queue family found.");
+
+    let gpu = unsafe {
+      adapter
+        .physical_device
+        .open(&[(queue_family, &[1.0])], gfx_hal::Features::empty())
+        .expect("Failed to open the device.")
+    };
+
+    return Self {
+      adapter,
+      gpu,
+      command_pool: None,
+    };
+  }
+
+  pub fn with_command_pool(self) -> Self {
+    let adapter = self.adapter;
+    let mut gpu = self.gpu;
+    let mut command_pool = unsafe {
+      gpu.device.create_command_pool(gpu.queue_groups.pop().unwrap().family, CommandPoolCreateFlags::empty()).expect("The GPU could not allocate a command pool because it is out of memory")
+    };
+
+    return Self {
+      adapter,
+      gpu,
+      command_pool: Some(command_pool),
+    };
+  }
+
+  /// Create a render pass with the current GPU.
+  pub fn create_render_pass(
+    &mut self,
+    resource_attachments: Option<Vec<Attachment>>,
+    render_subpasses: Option<Vec<SubpassDesc>>,
+    dependencies: Option<Vec<SubpassDependency>>,
+  ) -> B::RenderPass {
+    // Use attached resources or create a stub for pipeline compatibility
+    let attachments = match resource_attachments {
+      Some(attachments) => attachments,
+      None => {
+        vec![Attachment {
+          format: None,
+          samples: 1,
+          ops: AttachmentOps::new(
+            AttachmentLoadOp::Clear,
+            AttachmentStoreOp::Store,
+          ),
+          stencil_ops: AttachmentOps::DONT_CARE,
+          layouts: Layout::Undefined..Layout::Present,
+        }]
+      }
+    };
+
+    // Use attached render subpasses or create a stub for pipeline compatibility.
+    let subpasses = match render_subpasses {
+      Some(subpasses) => subpasses,
+      None => vec![SubpassDesc {
+        colors: &[(0, Layout::ColorAttachmentOptimal)],
+        depth_stencil: None,
+        inputs: &[],
+        resolves: &[],
+        preserves: &[],
+      }],
+    };
+
+    // Use attached dependencies or create a stub for pipeline compatibility.
+    let deps = match dependencies {
+      Some(deps) => deps,
+      None => vec![SubpassDependency {
+        accesses: Access::COLOR_ATTACHMENT_READ..Access::COLOR_ATTACHMENT_WRITE,
+        flags: Dependencies::empty(),
+        passes: None..None,
+        stages: PipelineStage::BOTTOM_OF_PIPE
+          ..PipelineStage::COLOR_ATTACHMENT_OUTPUT,
+      }],
+    };
+
+    return unsafe {
+      self
+				.gpu
+				.device
+				.create_render_pass(attachments.into_iter(), subpasses.into_iter(), deps.into_iter())
+				.expect("Your primary graphics card does not have enough memory for this render pass.")
+    };
+  }
+}
--- lambda/src/platform/gfx/mod.rs
+++ lambda/src/platform/gfx/mod.rs
@@ -1,63 +1,61 @@
 use gfx_hal::{
-  adapter::{
-    Adapter,
-    Gpu,
-  },
-  device::Device,
+  adapter::Adapter,
   format::{
     ChannelType,
     Format,
   },
-  image::{
-    Access,
-    Layout,
-  },
-  memory::Dependencies,
-  pass::{
-    Attachment,
-    AttachmentLoadOp,
-    AttachmentOps,
-    AttachmentStoreOp,
-    SubpassDependency,
-    SubpassDesc,
-  },
   prelude::QueueFamily,
-  pso::PipelineStage,
   window::Surface,
   Instance,
 };
 
 use crate::core::window::LambdaWindow;
 
-/// Create a Surface for a backend using a WinitP.
-/// TODO(vmarcella): The window pointer taken in should most likely be a
-/// generic.
-pub fn create_surface<B: gfx_hal::Backend>(
-  instance: &B::Instance,
-  window: &LambdaWindow,
-) -> B::Surface {
-  return unsafe {
-    // TODO(vmarcella): This currently just unwraps the surface
-    instance
-      .create_surface(window.winit_window_ref().unwrap())
-      .unwrap()
-  };
+pub mod gpu;
+
+pub struct GfxInstance<B: gfx_hal::Backend> {
+  instance: B::Instance,
 }
 
-pub fn find_supported_render_queue<'a, B: gfx_hal::Backend>(
-  surface: &'a B::Surface,
-  adapter: &'a Adapter<B>,
-) -> &'a B::QueueFamily {
-  return adapter
-    .queue_families
-    .iter()
-    .find(|family| {
-      let supports_queue = surface.supports_queue_family(family);
-      let supports_graphics = family.queue_type().supports_graphics();
+impl<B: gfx_hal::Backend> GfxInstance<B> {
+  pub fn new(name: &str) -> Self {
+    let instance = B::Instance::create(name, 1)
+      .expect("gfx backend not supported by the current platform");
 
-      supports_queue && supports_graphics
-    })
-    .unwrap();
+    return Self { instance };
+  }
+
+  /// Create a surface for a given lambda window using it's underlying
+  /// winit window handle.
+  /// TODO(vmarcella): Wrap up the B::Surface type to a custom type of our own.
+  pub fn create_surface(&self, window: &LambdaWindow) -> B::Surface {
+    return unsafe {
+      self
+        .instance
+        .create_surface(window.winit_window_ref().unwrap())
+        .unwrap()
+    };
+  }
+
+  // Open a connection to the primary GPU with an optional surface attached.
+  //
+  pub fn open_primary_gpu(
+    &self,
+    surface: Option<&B::Surface>,
+  ) -> gpu::GfxGpu<B> {
+    let adapter = self.instance.enumerate_adapters().remove(0);
+    return gpu::GfxGpu::<B>::new(
+      adapter,
+      gpu::RenderQueueType::Graphical,
+      surface,
+    );
+  }
+}
+
+// Create a graphical backend instance using the platforms default installed
+// graphical backend
+pub fn create_default_gfx_instance() -> GfxInstance<backend::Backend> {
+  return GfxInstance::<backend::Backend>::new("Lambda Application");
 }
 
 /// Finds the first supported color format or default to Rgba8Srgb.
@@ -77,61 +75,4 @@ pub fn find_supported_color_format<B: gfx_hal::Backend>(
     .into_iter()
     .find(|format| -> bool { format.base_format().1 == ChannelType::Srgb })
     .unwrap_or(default_format);
-}
-
-// Creates a render pass given a GPU and optional resources, subpasses, and
-// dependencies that you'd like to be specified for the pipeline.
-pub fn create_render_pass<B: gfx_hal::Backend>(
-  gpu: &mut Gpu<B>,
-  resource_attachments: Option<Vec<Attachment>>,
-  render_subpasses: Option<Vec<SubpassDesc>>,
-  dependencies: Option<Vec<SubpassDependency>>,
-) -> B::RenderPass {
-  // Use attached resources or create a stub for pipeline compatibility
-  let attachments = match resource_attachments {
-    Some(attachments) => attachments,
-    None => {
-      vec![Attachment {
-        format: None,
-        samples: 1,
-        ops: AttachmentOps::new(
-          AttachmentLoadOp::Clear,
-          AttachmentStoreOp::Store,
-        ),
-        stencil_ops: AttachmentOps::DONT_CARE,
-        layouts: Layout::Undefined..Layout::Present,
-      }]
-    }
-  };
-
-  // Use attached render subpasses or create a stub for pipeline compatibility.
-  let subpasses = match render_subpasses {
-    Some(subpasses) => subpasses,
-    None => vec![SubpassDesc {
-      colors: &[(0, Layout::ColorAttachmentOptimal)],
-      depth_stencil: None,
-      inputs: &[],
-      resolves: &[],
-      preserves: &[],
-    }],
-  };
-
-  // Use attached dependencies or create a stub for pipeline compatibility.
-  let deps = match dependencies {
-    Some(deps) => deps,
-    None => vec![SubpassDependency {
-      accesses: Access::COLOR_ATTACHMENT_READ..Access::COLOR_ATTACHMENT_WRITE,
-      flags: Dependencies::empty(),
-      passes: None..None,
-      stages: PipelineStage::BOTTOM_OF_PIPE
-        ..PipelineStage::COLOR_ATTACHMENT_OUTPUT,
-    }],
-  };
-
-  return unsafe {
-    gpu
-			.device
-			.create_render_pass(attachments.into_iter(), subpasses.into_iter(), deps.into_iter())
-			.expect("Your primary graphics card does not have enough memory for this render pass.")
-  };
 }
