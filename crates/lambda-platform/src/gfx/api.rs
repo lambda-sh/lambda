2bad987eb0a98aaeb4fb1a0f7da964b33c99ef8e
vmarcella <cenz@cenz.io>
2022-05-17 12:34:24 -0700

    [update] lambda to specify lambda-platform/detect-platform for fallback dependency configuration. This ensures that there will always be a graphical backend available if not specified.

--- crates/lambda-platform/src/gfx/api.rs
+++ crates/lambda-platform/src/gfx/api.rs
@@ -9,7 +9,8 @@ cfg_if::cfg_if! {
     pub use gfx_backend_vulkan as RenderingAPI;
   } else if #[cfg(feature = "with-dx12")] {
     pub use gfx_backend_vulkan as RenderingAPI;
+  } else if #[cfg(feature = "detect-platform")] {
+      pub use gfx_platform_backend as RenderingAPI;
   } else {
-    pub use gfx_backend_gl as RenderingAPI;
   }
 }
