5b3e68e98810dd350591bb7cc68651f0eb711e8a
vmarcella <cenz@cenz.io>
2022-10-15 12:44:45 -0700

    [add] documentation to the minimal demo.

--- tools/minimal/src/main.rs
+++ tools/minimal/src/main.rs
@@ -1,3 +1,8 @@
+//! Minimal application which configures a window & render context before
+//! starting the runtime. You can use this as a starting point for your own
+//! applications or to verify that your system is configured to run lambda
+//! applications correctly.
+
 use lambda::{
   core::runtime::start_runtime,
   runtimes::GenericRuntimeBuilder,
