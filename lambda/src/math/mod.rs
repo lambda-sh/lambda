9974b33c3665faed3a52151a09ac8f51431f0180
vmarcella <cenz@cenz.io>
2022-11-17 17:26:29 -0800

    [remove] free functions and implement all  vector traits and vector2 tests.

--- lambda/src/math/mod.rs
+++ lambda/src/math/mod.rs
@@ -52,68 +52,48 @@ impl Vector<f32> for Vector2 {
   }
 }
 
-impl Vector<f32> for Vector3 {
-  fn add(&self, other: &Self) -> Self {
-    todo!()
-  }
-
-  fn subtract(&self, other: &Self) -> Self {
-    todo!()
-  }
-
-  fn multiply(&self, other: &Self) -> Self {
-    todo!()
-  }
-
-  fn dot(&self, other: &Self) -> f64 {
-    todo!()
-  }
-
-  fn cross(&self, other: &Self) -> Self {
-    todo!()
-  }
-
-  fn length(&self) -> f64 {
-    todo!()
-  }
-
-  fn normalize(&self) -> Self {
-    todo!()
-  }
-}
-
-/// Compute the dot product of two `Vec2`.
-pub fn dot(a: Vector2, b: Vector2) -> f32 {
-  a.0 * b.0 + a.1 * b.1
-}
-
-/// Compute the length of a vector.
-pub fn length(a: Vector2) -> f32 {
-  return (a.0 * a.0 + a.1 * a.1).sqrt();
-}
-
-/// Normalized vector.
-pub fn normalize(a: Vector2) -> Vector2 {
-  let length = length(a);
-
-  if length == 0.0 {
-    return a;
-  }
-
-  return (a.0 / length, a.1 / length);
-}
-
 //  ------------------------------- VECTOR3 -----------------------------------
 
 pub type Vector3 = (f32, f32, f32);
 
-/// Compute the cross product of two `Vec3`.
-pub fn cross(a: Vector3, b: Vector3) -> Vector3 {
-  return (
-    a.1 * b.2 - a.2 * b.1,
-    a.2 * b.0 - a.0 * b.2,
-    a.0 * b.1 - a.1 * b.0,
-  );
+impl Vector<f32> for Vector3 {
+  fn add(&self, other: &Self) -> Self {
+    return (self.0 + other.0, self.1 + other.1, self.2 + other.2);
+  }
+
+  fn subtract(&self, other: &Self) -> Self {
+    return (self.0 - other.0, self.1 - other.1, self.2 - other.2);
+  }
+
+  fn multiply(&self, other: &Self) -> Self {
+    return (self.0 * other.0, self.1 * other.1, self.2 * other.2);
+  }
+
+  fn dot(&self, other: &Self) -> f32 {
+    return self.0 * other.0 + self.1 * other.1 + self.2 * other.2;
+  }
+
+  fn cross(&self, other: &Self) -> Self {
+    return (
+      self.1 * other.2 - self.2 * other.1,
+      self.2 * other.0 - self.0 * other.2,
+      self.0 * other.1 - self.1 * other.0,
+    );
+  }
+
+  fn length(&self) -> f32 {
+    return (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt();
+  }
+
+  fn normalize(&self) -> Self {
+    let length = self.length();
+
+    if length == 0.0 {
+      return (0.0, 0.0, 0.0);
+    }
+
+    return (self.0 / length, self.1 / length, self.2 / length);
+  }
 }
 
 //  ------------------------------- VECTOR4 -----------------------------------
@@ -156,27 +136,25 @@ pub fn translate(
 
 #[cfg(test)]
 mod tests {
+  use super::*;
 
   #[test]
   fn test_dot() {
-    assert_eq!(super::dot((1.0, 2.0), (3.0, 4.0)), 11.0);
+    assert_eq!((1.0, 2.0).dot(&(3.0, 4.0)), 11.0);
   }
 
   #[test]
   fn test_cross() {
-    assert_eq!(
-      super::cross((1.0, 2.0, 3.0), (4.0, 5.0, 6.0)),
-      (-3.0, 6.0, -3.0)
-    );
+    assert_eq!((1.0, 2.0, 3.0).cross(&(4.0, 5.0, 6.0)), (-3.0, 6.0, -3.0));
   }
 
   #[test]
   fn test_length() {
-    assert_eq!(super::length((3.0, 4.0)), 5.0);
+    assert_eq!((3.0, 4.0).length(), 5.0);
   }
 
   #[test]
   fn test_normalize() {
-    assert_eq!(super::normalize((3.0, 4.0)), (0.6, 0.8));
+    assert_eq!((3.0, 4.0).normalize(), (0.6, 0.8));
   }
 }
