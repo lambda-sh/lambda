96af5e818a65fd47a38a0c927df5bc4d40b93f91
vmarcella <vmarcella@microsoft.com>
2022-11-26 15:09:48 -0800

    [update] matrix traits and remove redundant traits.

--- lambda/src/math/matrix.rs
+++ lambda/src/math/matrix.rs
@@ -13,7 +13,7 @@ pub enum Axes {
 }
 
 /// Common Matrix operations that can be implemented by any matrix like type.
-pub trait Matrix<T, V: Vector<T>> {
+pub trait MatrixOperations<T, V: Vector<T>> {
   fn identity() -> Self;
   fn transform(&self, vector: &V) -> V;
   fn rotate(&self, vector: &V, axis: Axes) -> V;
@@ -21,51 +21,12 @@ pub trait Matrix<T, V: Vector<T>> {
   fn multiply(&self, other: &Self) -> Self;
 }
 
-pub trait CanMultiplyAgainst<Left, Right> {
-  fn can_multiply_against(left: &Left, right: &Right) -> bool;
-}
-
-/// Support for Matrix multliplication.
-pub trait MatrixMultiply<Right, Result> {
-  fn multiply(&self, other: Right) -> Result;
-}
-
-impl MatrixMultiply<Vector4, Vector3> for Matrix4x4f {
-  fn multiply(&self, other: Vector4) -> Vector3 {
-    let x = self.0 .0 * other.0
-      + self.1 .0 * other.1
-      + self.2 .0 * other.2
-      + self.3 .0 * other.3;
-
-    let y = self.0 .1 * other.0
-      + self.1 .1 * other.1
-      + self.2 .1 * other.2
-      + self.3 .1 * other.3;
-
-    let z = self.0 .2 * other.0
-      + self.1 .2 * other.1
-      + self.2 .2 * other.2
-      + self.3 .2 * other.3;
-
-    let w = self.0 .3 * other.0
-      + self.1 .3 * other.1
-      + self.2 .3 * other.2
-      + self.3 .3 * other.3;
-
-    if w != 0.0 && w != 1.0 {
-      return (x / w, y / w, z / w);
-    }
-
-    return (x, y, z);
-  }
-}
-
 pub type Matrix1x3f = Vector3;
 pub type Matrix2x3f = (Vector3, Vector3);
 pub type Matrix3x3f = (Vector3, Vector3, Vector3);
 pub type Matrix4x3f = (Vector3, Vector3, Vector3, Vector3);
 
-impl Matrix<f32, Vector3> for Matrix4x3f {
+impl MatrixOperations<f32, Vector3> for Matrix4x3f {
   fn identity() -> Self {
     todo!()
   }
@@ -87,12 +48,28 @@ impl Matrix<f32, Vector3> for Matrix4x3f {
   }
 }
 
+pub struct Matrix<const columns: usize, const rows: usize> {
+  rows: usize,
+  columns: usize,
+  data: [[f32; columns]; rows],
+}
+
+impl<const columns: usize, const rows: usize> Matrix<columns, rows> {
+  pub fn new(data: [[f32; columns]; rows]) -> Self {
+    Self {
+      rows,
+      columns,
+      data,
+    }
+  }
+}
+
 pub type Matrix1x4f = Vector4;
 pub type Matrix2x4f = (Vector4, Vector4);
 pub type Matrix3x4f = (Vector4, Vector4, Vector4);
 pub type Matrix4x4f = (Vector4, Vector4, Vector4, Vector4);
 
-impl Matrix<f32, Vector4> for Matrix3x4f {
+impl MatrixOperations<f32, Vector4> for Matrix3x4f {
   fn identity() -> Self {
     todo!()
   }
@@ -157,7 +134,7 @@ impl Matrix<f32, Vector4> for Matrix3x4f {
   }
 }
 
-impl Matrix<f32, Vector4> for Matrix4x4f {
+impl MatrixOperations<f32, Vector4> for Matrix4x4f {
   fn identity() -> Self {
     return (
       (1.0, 0.0, 0.0, 0.0),
