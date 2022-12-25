3798cfe6f6b949d19bce91eb34489a5a61c4d46f
vmarcella <cenz@cenz.io>
2022-12-25 15:33:14 -0500

    [fix] cross product implementation and add tests for the dot product & cross product.

--- lambda/src/math/vector.rs
+++ lambda/src/math/vector.rs
@@ -46,6 +46,12 @@ where
   }
 
   fn dot(&self, other: &Self) -> Self::Scalar {
+    assert_eq!(
+      self.as_ref().len(),
+      other.as_ref().len(),
+      "Vectors must be the same length"
+    );
+
     let mut result = 0.0;
     for (a, b) in self.as_ref().iter().zip(other.as_ref().iter()) {
       result += a * b;
@@ -54,15 +60,27 @@ where
   }
 
   fn cross(&self, other: &Self) -> Self {
+    assert_eq!(
+      self.as_ref().len(),
+      other.as_ref().len(),
+      "Vectors must be the same length"
+    );
+
     let mut result = Self::default();
-    self
-      .as_ref()
-      .iter()
-      .zip(other.as_ref().iter())
-      .enumerate()
-      .for_each(|(i, (a, b))| {
-        result.as_mut()[i] = a * b;
-      });
+    let a = self.as_ref();
+    let b = other.as_ref();
+
+    // TODO: This is only for 3D vectors
+    match a.len() {
+      3 => {
+        result.as_mut()[0] = a[1] * b[2] - a[2] * b[1];
+        result.as_mut()[1] = a[2] * b[0] - a[0] * b[2];
+        result.as_mut()[2] = a[0] * b[1] - a[1] * b[0];
+      }
+      _ => {
+        panic!("Cross product is only defined for 3 dimensional vectors.")
+      }
+    }
     return result;
   }
 
@@ -129,5 +147,34 @@ mod tests {
 
     let result = a.scale(scalar);
     assert_eq!(result, b);
+  }
+
+  #[test]
+  fn dot_product() {
+    let a = [1.0, 2.0, 3.0];
+    let b = [4.0, 5.0, 6.0];
+    let c = 32.0;
+
+    let result = a.dot(&b);
+    assert_eq!(result, c);
+  }
+
+  #[test]
+  fn cross_product() {
+    let a = [1.0, 2.0, 3.0];
+    let b = [4.0, 5.0, 6.0];
+    let c = [-3.0, 6.0, -3.0];
+
+    let result = a.cross(&b);
+    assert_eq!(result, c);
+  }
+
+  #[test]
+  fn cross_product_fails_for_non_3d_vectors() {
+    let a = [1.0, 2.0];
+    let b = [4.0, 5.0];
+
+    let result = std::panic::catch_unwind(|| a.cross(&b));
+    assert!(result.is_err());
   }
 }
