286fee621a77c12b8587b41ab186754f06ef0164
vmarcella <cenz@cenz.io>
2023-01-25 17:24:21 -0800

    [update] render API to better handle detecting the default platform API.

--- crates/lambda-platform/src/gfx/api.rs
+++ crates/lambda-platform/src/gfx/api.rs
@@ -2,17 +2,23 @@
 //! implementations to use.
 
 cfg_if::cfg_if! {
-  if #[cfg(feature = "gfx-with-gl")] {
-    pub use gfx_backend_gl as RenderingAPI;
-  } else if #[cfg(feature = "gfx-with-vulkan")] {
-    pub use gfx_backend_vulkan as RenderingAPI;
-  } else if #[cfg(feature = "gfx-with-metal")] {
-    pub use gfx_backend_metal as RenderingAPI;
-  } else if #[cfg(feature = "gfx-with-dx11")] {
-    pub use gfx_backend_dx11 as RenderingAPI;
-  } else if #[cfg(feature = "gfx-with-dx12")] {
+if #[cfg(feature = "gfx-with-gl")] {
+  pub use gfx_backend_gl as RenderingAPI;
+} else if #[cfg(feature = "gfx-with-metal")] {
+  pub use gfx_backend_metal as RenderingAPI;
+} else if #[cfg(feature = "gfx-with-vulkan")] {
+  pub use gfx_backend_vulkan as RenderingAPI;
+} else if #[cfg(feature = "gfx-with-dx11")] {
+  pub use gfx_backend_dx11 as RenderingAPI;
+} else if #[cfg(feature = "gfx-with-dx12")] {
+  pub use gfx_backend_dx12 as RenderingAPI;
+} else if #[cfg(all(feature = "detect-platform", windows))] {
     pub use gfx_backend_dx12 as RenderingAPI;
-  } else if #[cfg(feature = "detect-platform")] {
-      pub use gfx_platform_backend as RenderingAPI;
-  } else {}
+} else if #[cfg(all(feature = "detect-platform", target_os = "macos"))] {
+    pub use gfx_backend_metal as RenderingAPI;
+  } else if #[cfg(all(feature = "detect-platform", unix, not(target_os = "macos")))] {
+    pub use gfx_backend_vulkan as RenderingAPI;
+  } else {
+    panic!("No supported GPU API found for the current platform.");
+  }
 }
