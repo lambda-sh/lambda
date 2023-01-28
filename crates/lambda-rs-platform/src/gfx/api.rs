ee0115b34aaefd544e637958707b5f015d0c1119
vmarcella <cenz@cenz.io>
2023-01-27 19:21:55 -0800

    [update] default dependency configurations.

--- crates/lambda-rs-platform/src/gfx/api.rs
+++ crates/lambda-rs-platform/src/gfx/api.rs
@@ -2,19 +2,18 @@
 //! implementations to use.
 
 cfg_if::cfg_if! {
-if #[cfg(feature = "gfx-with-gl")] {
+if #[cfg(any(feature = "gfx-with-gl", all(feature = "detect-platform", unix, not(target_os="macos")) ))] {
   pub use gfx_backend_gl as RenderingAPI;
-} else if #[cfg(feature = "gfx-with-metal")] {
+} else if #[cfg(any(feature = "gfx-with-metal", all(feature = "detect-platform", target_os="macos")))] {
   pub use gfx_backend_metal as RenderingAPI;
 } else if #[cfg(feature = "gfx-with-vulkan")] {
   pub use gfx_backend_vulkan as RenderingAPI;
 } else if #[cfg(feature = "gfx-with-dx11")] {
   pub use gfx_backend_dx11 as RenderingAPI;
-} else if #[cfg(feature = "gfx-with-dx12")] {
+} else if #[cfg(any(feature = "gfx-with-dx12", all(windows, feature = "detect-platform")))] {
   pub use gfx_backend_dx12 as RenderingAPI;
-} else if #[cfg(all(feature = "detect-platform"))] {
-  pub use default_backend as RenderingAPI;
 } else {
     pub use gfx_backend_empty as RenderingAPI;
+    println!("[WARN] No rendering backend specified, using empty backend.");
   }
 }
